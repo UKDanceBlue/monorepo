1e4269e5e8ec9d1149969a4b13dc88d67997028b
Joshua Tag Howard <tag@jthoward.dev>
2023-10-23 19:28:11 -0400

    Fill out the NotificationResource class

--- packages/common/lib/api/graphql/object-types/Notification.ts
+++ packages/common/lib/api/graphql/object-types/Notification.ts
@@ -1,11 +1,76 @@
-import { Field, ID, ObjectType } from "type-graphql";
+import { Field, ID, ObjectType, registerEnumType } from "type-graphql";
 
 import { Resource } from "./Resource.js";
+/*
+interface FirestoreNotification {
+  body: string;
+  sendTime: string;
+  sound?: string;
+  title: string;
+  payload?: Record<string, unknown>;
+}
+*/
+
+const NotificationPayloadPresentationType = {
+  URL: "URL",
+  IN_APP_VIEW: "IN_APP_VIEW",
+  INFO_POPUP: "INFO_POPUP",
+} as const;
+type NotificationPayloadType =
+  (typeof NotificationPayloadPresentationType)[keyof typeof NotificationPayloadPresentationType];
+
+registerEnumType(NotificationPayloadPresentationType, {
+  name: "NotificationPayloadType",
+  description:
+    "The type of presentation for the notification, URL skips the app and opens a URL directly, IN_APP_VIEW opens a webview, and INFO_POPUP shows a popup",
+});
+
+@ObjectType()
+export class NotificationPayload {
+  @Field(() => NotificationPayloadPresentationType)
+  presentation!: NotificationPayloadType;
+
+  @Field({
+    nullable: true,
+    description:
+      "A URL related to the notification, opened immediately for presentation type URL, opened in a webview for presentation type IN_APP_VIEW, and shown as a button for presentation type INFO_POPUP",
+  })
+  url?: string;
+
+  @Field({
+    nullable: true,
+    description:
+      "A title for the notification, ignored for presentation type URL, shown with the webview for presentation type IN_APP_VIEW, and shown at the top of the popup for presentation type INFO_POPUP",
+  })
+  title?: string;
+
+  @Field({
+    nullable: true,
+    description:
+      "Only shown for presentation type INFO_POPUP, shown at the bottom of the popup",
+  })
+  message?: string;
+}
 
 @ObjectType()
 export class NotificationResource extends Resource {
   @Field(() => ID)
   uuid!: string;
+
+  @Field()
+  body!: string;
+
+  @Field()
+  sendTime!: string;
+
+  @Field({ nullable: true })
+  sound?: string;
+
+  @Field()
+  title!: string;
+
+  @Field(() => NotificationPayload, { nullable: true })
+  payload?: NotificationPayload;
 
   public getUniqueId(): string {
     return this.uuid;
