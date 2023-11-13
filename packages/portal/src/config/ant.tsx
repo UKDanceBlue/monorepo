f2ca7b71bc2056d30655c60283b3122c22eac2f3
Joshua Tag Howard <tag@jthoward.dev>
2023-11-13 03:55:52 -0500

    Add disabled dark mode support

---
+++ packages/portal/src/config/ant.tsx
@@ -0,0 +1,37 @@
+import type { ThemeConfig } from "antd";
+import { ConfigProvider, theme } from "antd";
+import { useState } from "react";
+
+function makeAntDesignTheme({ dark }: { dark: boolean }): ThemeConfig {
+  return {
+    token: {
+      // Seed Token
+      colorPrimary: "#0032A0",
+      borderRadius: 2,
+
+      // Alias Token
+      colorBgContainer: "#f6ffed",
+    },
+    algorithm: dark ? theme.darkAlgorithm : theme.defaultAlgorithm,
+  };
+}
+
+export function AntConfigProvider({ children }: { children: React.ReactNode }) {
+  const [dark] = useState(false);
+
+  // useEffect(() => {
+  //   const darkModePreference = window.matchMedia(
+  //     "(prefers-color-scheme: dark)"
+  //   );
+  //   darkModePreference.addEventListener("change", (e) => {
+  //     setDark(e.matches);
+  //   });
+  //   setDark(darkModePreference.matches);
+  // }, []);
+
+  return (
+    <ConfigProvider theme={makeAntDesignTheme({ dark })}>
+      {children}
+    </ConfigProvider>
+  );
+}
--- packages/portal/src/main.tsx
+++ packages/portal/src/main.tsx
@@ -1,10 +1,10 @@
 import { RouterProvider } from "@tanstack/react-router";
-import { App as AntApp, ConfigProvider } from "antd";
+import { App as AntApp } from "antd";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider as UrqlProvider } from "urql";
 
-import { antDesignTheme } from "./config/ant.ts";
+import { AntConfigProvider } from "./config/ant.tsx";
 import { urqlClient } from "./config/urql.ts";
 import { router } from "./routing/router.ts";
 
@@ -12,12 +12,12 @@ import "normalize.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ConfigProvider theme={antDesignTheme}>
+    <AntConfigProvider>
       <AntApp>
         <UrqlProvider value={urqlClient}>
           <RouterProvider router={router} />
         </UrqlProvider>
       </AntApp>
-    </ConfigProvider>
+    </AntConfigProvider>
   </StrictMode>
 );
