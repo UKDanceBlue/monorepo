6c191ab53f0e665621f462cb079efc6da6632598
Joshua Tag Howard <tag@jthoward.dev>
2023-11-16 03:53:08 -0500

    Extract checkAuthorization to common

--- packages/common/lib/api/graphql/object-types/authorization.ts
+++ packages/common/lib/api/graphql/object-types/authorization.ts
@@ -1,19 +1,18 @@
 import { Authorized } from "type-graphql";
 
-import type {
-  AccessLevel,
-  Authorization,
-  CommitteeRole,
-  DbRole,
-} from "../../../index.js";
+import type { AccessLevel, Authorization, DbRole } from "../../../index.js";
+import { CommitteeRole } from "../../../index.js";
 
 export interface AuthorizationRule extends Partial<Authorization> {
   /**
-   * Exact DanceBlue community role. This does not have a minDbRole equivalent
-   * as there isn't a defined hierarchy for these roles. If you need to check
-   * for a minimum role, you will need to have multiple rules.
+   * Exact DanceBlue role, cannot be used with minDbRole
    */
   dbRole?: DbRole;
+  /**
+   * The rules for this are as follows:
+   * Committee > TeamCaptain > TeamMember > Public > None
+   */
+  minDbRole?: DbRole;
   /**
    * Exact committee role, cannot be used with minCommitteeRole
    */
@@ -41,6 +40,74 @@ export interface AuthorizationRule extends Partial<Authorization> {
    * Should usually be avoided, but can be used for more complex authorization rules
    */
   custom?: (authorization: Authorization) => boolean;
+}
+
+export function checkAuthorization(
+  role: AuthorizationRule,
+  authorization: Authorization
+) {
+  if (role.minDbRole != null && role.dbRole != null) {
+    throw new TypeError(`Cannot specify both dbRole and minDbRole.`);
+  }
+  if (role.minCommitteeRole != null && role.committeeRole != null) {
+    throw new TypeError(
+      `Cannot specify both committeeRole and minCommitteeRole.`
+    );
+  }
+  if (role.committeeIdentifier != null && role.committeeIdentifiers != null) {
+    throw new TypeError(
+      `Cannot specify both committeeIdentifier and committeeIdentifiers.`
+    );
+  }
+
+  let matches = true;
+
+  // DB role
+  if (role.dbRole != null) {
+    matches &&= authorization.dbRole === role.dbRole;
+  }
+
+  // Committee role
+  if (role.committeeRole != null) {
+    matches &&= authorization.committeeRole === role.committeeRole;
+  }
+  if (role.minCommitteeRole != null) {
+    switch (role.minCommitteeRole) {
+      case CommitteeRole.Chair: {
+        matches &&= authorization.committeeRole === CommitteeRole.Chair;
+        break;
+      }
+      case CommitteeRole.Coordinator: {
+        matches &&=
+          authorization.committeeRole === CommitteeRole.Chair ||
+          authorization.committeeRole === CommitteeRole.Coordinator;
+        break;
+      }
+      case CommitteeRole.Member: {
+        matches &&=
+          authorization.committeeRole === CommitteeRole.Chair ||
+          authorization.committeeRole === CommitteeRole.Coordinator ||
+          authorization.committeeRole === CommitteeRole.Member;
+        break;
+      }
+    }
+  }
+
+  // Committee identifier(s)
+  if (role.committeeIdentifier != null) {
+    matches &&= authorization.committeeIdentifier === role.committeeIdentifier;
+  }
+  if (role.committeeIdentifiers != null) {
+    matches &&= role.committeeIdentifiers.includes(
+      String(authorization.committeeIdentifier)
+    );
+  }
+
+  // Custom auth checker
+  if (role.custom != null) {
+    matches &&= role.custom(authorization);
+  }
+  return matches;
 }
 
 export type AuthorizationRuleOrAccessLevel = AuthorizationRule | AccessLevel;
--- packages/server/src/resolvers/authChecker.ts
+++ packages/server/src/resolvers/authChecker.ts
@@ -1,5 +1,5 @@
 import type { AuthorizationRuleOrAccessLevel } from "@ukdanceblue/common";
-import { CommitteeRole } from "@ukdanceblue/common";
+import { checkAuthorization } from "@ukdanceblue/common";
 import type { AuthChecker } from "type-graphql";
 
 import { authorizationOverride } from "../environment.js";
@@ -47,45 +47,16 @@ export const customAuthChecker: AuthChecker<
         return true;
       }
     } else if (typeof role === "object") {
-      let matches = true;
-
-      // DB role
-      if (role.dbRole != null) {
-        matches &&= authorization.dbRole === role.dbRole;
+      if (role.dbRole != null && role.minDbRole != null) {
+        throw new TypeError(
+          `Cannot specify both dbRole and minDbRole. See ${info.parentType.name}.${info.fieldName}`
+        );
       }
-
-      // Committee role
       if (role.committeeRole != null && role.minCommitteeRole != null) {
         throw new TypeError(
           `Cannot specify both committeeRole and minCommitteeRole. See ${info.parentType.name}.${info.fieldName}`
         );
       }
-      if (role.committeeRole != null) {
-        matches &&= authorization.committeeRole === role.committeeRole;
-      }
-      if (role.minCommitteeRole != null) {
-        switch (role.minCommitteeRole) {
-          case CommitteeRole.Chair: {
-            matches &&= authorization.committeeRole === CommitteeRole.Chair;
-            break;
-          }
-          case CommitteeRole.Coordinator: {
-            matches &&=
-              authorization.committeeRole === CommitteeRole.Chair ||
-              authorization.committeeRole === CommitteeRole.Coordinator;
-            break;
-          }
-          case CommitteeRole.Member: {
-            matches &&=
-              authorization.committeeRole === CommitteeRole.Chair ||
-              authorization.committeeRole === CommitteeRole.Coordinator ||
-              authorization.committeeRole === CommitteeRole.Member;
-            break;
-          }
-        }
-      }
-
-      // Committee identifier(s)
       if (
         role.committeeIdentifier != null &&
         role.committeeIdentifiers != null
@@ -94,25 +65,8 @@ export const customAuthChecker: AuthChecker<
           `Cannot specify both committeeIdentifier and committeeIdentifiers. See ${info.parentType.name}.${info.fieldName}`
         );
       }
-      if (role.committeeIdentifier != null) {
-        matches &&=
-          authorization.committeeIdentifier === role.committeeIdentifier;
-      }
-      if (role.committeeIdentifiers != null) {
-        matches &&= role.committeeIdentifiers.includes(
-          String(authorization.committeeIdentifier)
-        );
-      }
 
-      // Custom auth checker
-      if (role.custom != null) {
-        matches &&= role.custom(authorization);
-      }
-
-      // Final result
-      if (matches) {
-        return true;
-      }
+      return checkAuthorization(role, authorization);
     } else {
       throw new TypeError(
         `Invalid role: ${JSON.stringify(role as unknown)}. See ${
