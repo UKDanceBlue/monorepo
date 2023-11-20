9904218daba5143f1397b4d21928e8d1066eedec
Joshua Tag Howard <tag@jthoward.dev>
2023-11-19 23:02:21 -0500

    Add TeamCreator and TeamEditor components to CreateTeamPage and EditTeamPage respectively

--- packages/portal/src/pages/teams/spirit/create-team/CreateTeamPage.tsx
+++ packages/portal/src/pages/teams/spirit/create-team/CreateTeamPage.tsx
@@ -1,7 +1,10 @@
+import { TeamCreator } from "@elements/forms/team/create/TeamCreator";
+
 export function CreateTeamPage() {
   return (
     <div>
       <h1>Create Team</h1>
+      <TeamCreator />
     </div>
   );
 }
--- packages/portal/src/pages/teams/spirit/single-team/edit-team/EditTeamPage.tsx
+++ packages/portal/src/pages/teams/spirit/single-team/edit-team/EditTeamPage.tsx
@@ -1,7 +1,37 @@
+import { TeamEditor } from "@elements/forms/team/edit/TeamEditor";
+import { useQueryStatusWatcher } from "@hooks/useQueryStatusWatcher";
+import { useParams } from "@tanstack/react-router";
+import { graphql } from "@ukdanceblue/common/graphql-client-admin";
+import { useQuery } from "urql";
+
+const viewTeamPageDocument = graphql(/* GraphQL */ `
+  query EditTeamPage($uuid: String!) {
+    team(uuid: $uuid) {
+      data {
+        ...TeamEditorFragment
+      }
+    }
+  }
+`);
+
 export function EditTeamPage() {
+  const { teamId } = useParams({ from: "/teams/$teamId/edit" });
+
+  const [{ data, fetching, error }, refetchTeam] = useQuery({
+    query: viewTeamPageDocument,
+    variables: { uuid: teamId },
+  });
+
+  useQueryStatusWatcher({
+    error,
+    fetching,
+    loadingMessage: "Loading team...",
+  });
+
   return (
     <div>
       <h1>Edit Team</h1>
+      <TeamEditor teamFragment={data?.team.data} refetchTeam={refetchTeam} />
     </div>
   );
 }
