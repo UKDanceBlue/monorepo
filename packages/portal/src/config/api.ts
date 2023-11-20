e59c65a14ef97d8b20f1a4db95ad0425e42d1f41
Joshua Tag Howard <tag@jthoward.dev>
2023-11-20 14:45:56 -0500

    Fix typo in API_BASE_URL

--- packages/portal/src/config/api.ts
+++ packages/portal/src/config/api.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = window.location.href.includes("loclhost")
+export const API_BASE_URL = window.location.href.includes("localhost")
   ? "http://localhost:8000"
   : "https://app.danceblue.org";
