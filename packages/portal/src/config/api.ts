081d6426abf0f43bae9dad937e3782847240c1e0
Joshua Tag Howard <tag@jthoward.dev>
2023-11-20 06:15:34 -0500

    Fix API_BASE_URL condition

--- packages/portal/src/config/api.ts
+++ packages/portal/src/config/api.ts
@@ -1,1 +1,3 @@
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = window.location.href.includes("loclhost")
+  ? "http://localhost:8000"
+  : "https://app.danceblue.org";
