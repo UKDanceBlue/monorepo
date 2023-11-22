d5b15be56fe8c576d6a791adcc4fa6c6741023f1
Joshua Tag Howard <tag@jthoward.dev>
2023-11-21 21:30:00 -0500

    Update API_BASE_URL in config file

--- packages/portal/src/config/api.ts
+++ packages/portal/src/config/api.ts
@@ -1,3 +1,4 @@
-export const API_BASE_URL = window.location.href.includes("localhost")
-  ? "http://localhost:8000"
-  : "https://app.danceblue.org";
+export const API_BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:8000"
+    : "https://app.danceblue.org";
