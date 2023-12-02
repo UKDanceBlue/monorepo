b32a5ec272c2961b8e63fc8cebaf7b058408b8bd
Joshua Tag Howard <tag@jthoward.dev>
2023-12-02 04:10:55 -0500

    Add anonymous login route

---
+++ packages/server/src/routes/api/auth/anonymous.ts
@@ -0,0 +1,49 @@
+import { AccessLevel, AuthSource, DbRole } from "@ukdanceblue/common";
+import type { Context } from "koa";
+import { DateTime } from "luxon";
+
+import { makeUserJwt } from "../../../lib/auth/index.js";
+
+export const anonymousLogin = (ctx: Context) => {
+  let redirectTo = "/";
+  const queryRedirectTo = Array.isArray(ctx.query.redirectTo)
+    ? ctx.query.redirectTo[0]
+    : ctx.query.redirectTo;
+  if (queryRedirectTo && queryRedirectTo.length > 0) {
+    redirectTo = queryRedirectTo;
+  } else {
+    return ctx.throw("Missing redirectTo query parameter", 400);
+  }
+  let setCookie = false;
+  let sendToken = false;
+  const returning = Array.isArray(ctx.query.returning)
+    ? ctx.query.returning
+    : [ctx.query.returning];
+  if (returning.includes("cookie")) {
+    setCookie = true;
+  }
+  if (returning.includes("token")) {
+    sendToken = true;
+  }
+
+  const jwt = makeUserJwt(
+    {
+      auth: {
+        accessLevel: AccessLevel.Public,
+        dbRole: DbRole.Public,
+      },
+    },
+    AuthSource.Anonymous
+  );
+  if (setCookie) {
+    ctx.cookies.set("token", jwt, {
+      httpOnly: true,
+      sameSite: "lax",
+      expires: DateTime.utc().plus({ weeks: 2 }).toJSDate(),
+    });
+  }
+  if (sendToken) {
+    redirectTo = `${redirectTo}?token=${encodeURIComponent(jwt)}`;
+  }
+  return ctx.redirect(redirectTo);
+};
--- packages/server/src/routes/api/auth/index.ts
+++ packages/server/src/routes/api/auth/index.ts
@@ -1,6 +1,7 @@
 import Router from "@koa/router";
 import { koaBody } from "koa-body";
 
+import { anonymousLogin } from "./anonymous.js";
 import { login } from "./login.js";
 import { oidcCallback } from "./oidcCallback.js";
 
@@ -32,5 +33,7 @@ authApiRouter.post(
 );
 
 authApiRouter.get("/login", login);
+
+authApiRouter.get("/anonymous", anonymousLogin);
 
 export default authApiRouter;
