b77df98fcfc21c01376d2dfb58d125a05a997b42
Joshua Tag Howard <tag@jthoward.dev>
2023-12-28 19:38:43 -0500

    Add a migration for configuration

---
+++ packages/server/src/migrations/003_configurationValues.ts
@@ -0,0 +1,69 @@
+/*
+Add:
+
+    value: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    validAfter: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    validUntil: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+
+to `configuration` table.
+*/
+
+import type { MigrationParams } from "umzug";
+
+import type { MigrationContext } from "./migrationContext.js";
+
+export async function up({
+  context: { queryInterface, DataTypes },
+}: MigrationParams<MigrationContext>) {
+  await queryInterface.addColumn("configuration", "value", {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  });
+
+  await queryInterface.addColumn("configuration", "validAfter", {
+    type: DataTypes.DATE,
+    allowNull: true,
+  });
+
+  await queryInterface.addColumn("configuration", "validUntil", {
+    type: DataTypes.DATE,
+    allowNull: true,
+  });
+
+  await queryInterface.addColumn("configuration", "createdAt", {
+    type: DataTypes.DATE,
+    allowNull: false,
+  });
+
+  await queryInterface.addColumn("configuration", "updatedAt", {
+    type: DataTypes.DATE,
+    allowNull: false,
+  });
+
+  await queryInterface.sequelize.query(`
+    UPDATE configuration
+    SET
+      value = '[]',
+      validAfter = NULL,
+      validUntil = NULL,
+      createdAt = NOW(),
+      updatedAt = NOW()
+  `);
+}
