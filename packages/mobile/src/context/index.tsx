0bdc3d754786f2a08f8fa4dbc40c39716ec42bc0
Joshua Tag Howard <tag@jthoward.dev>
2023-12-30 01:37:15 -0500

    Strip out remaining Firebase auth/user logic

--- packages/mobile/src/context/index.tsx
+++ packages/mobile/src/context/index.tsx
@@ -1,11 +1,9 @@
 import { View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import { AuthDataProvider } from "./auth";
 import { DeviceDataProvider } from "./device";
 import { FirebaseProvider } from "./firebase";
 import { LoadingWrapper } from "./loading";
-import { UserDataProvider } from "./user";
 
 export { useAuthData } from "./auth";
 export { useDeviceData } from "./device";
@@ -21,15 +19,11 @@ export const CombinedContext = ({
   <LoadingWrapper>
     <FirebaseProvider>
       <DeviceDataProvider>
-          <AuthDataProvider>
-            <UserDataProvider>
-              <GestureHandlerRootView>
-                <View style={{ minHeight: "100%", minWidth: "100%" }}>
-                  {children}
-                </View>
-              </GestureHandlerRootView>
-            </UserDataProvider>
-          </AuthDataProvider>
+        <GestureHandlerRootView>
+          <View style={{ minHeight: "100%", minWidth: "100%" }}>
+            {children}
+          </View>
+        </GestureHandlerRootView>
       </DeviceDataProvider>
     </FirebaseProvider>
   </LoadingWrapper>
