f8d5179488360cfce6eb06efa8a8356a9493d0a2
Tag Howard <tag@jthoward.dev>
2024-01-18 15:38:33 -0800

    Sdk 50 release (#52)
    * Update to release version of SDK 50
    
    * Add an example for how we can add expo-font to app.config.json
    
    * Bump version

--- packages/mobile/app.config.ts
+++ packages/mobile/app.config.ts
@@ -20,17 +20,17 @@ To bump the app's version:
 */
 const bundleVersion: Version = {
   major: 2,
-  minor: 4,
+  minor: 5,
   patch: 0,
 } as const;
 const nativeVersion: Version = {
   major: 2,
-  minor: 4,
+  minor: 5,
   patch: 0,
 } as const;
 
 // Both the sum of version.patch + buildsThisVersion and the sum of baseBuildCount + buildsThisVersion must increase each time a native build is submitted.
-const baseBuildCount = 39;
+const baseBuildCount = 40;
 const buildsThisVersion = 1;
 
 /*
