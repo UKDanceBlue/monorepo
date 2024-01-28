3cf5a7c514d5b73e96d4543eca2df27c87943d5d
Joshua Tag Howard <tag@jthoward.dev>
2024-01-28 16:29:55 -0500

    Run ESLint auto-fix

--- packages/mobile/src/navigation/root/tab/ExplorerScreen/ExplorerItem.tsx
+++ packages/mobile/src/navigation/root/tab/ExplorerScreen/ExplorerItem.tsx
@@ -1,129 +1,188 @@
-import { FontAwesome5 } from "@expo/vector-icons";
 // import DBLogoCondensed from "../../../../../assets/svgs/DBLogoCondensed";
-import DBRibbon from "../../../../../assets/svgs/DBRibbon";
-import { PixelRatio, useWindowDimensions } from "react-native";
-import { Box, Button, HStack, Text, View } from "native-base";
-import { universalCatch } from "@common/logging";
-import { openURL } from "expo-linking";
 import AudioPlayer from "@common/components/AudioPlayer";
+import { universalCatch } from "@common/logging";
 import { showMessage } from "@common/util/alertUtils";
+import { FontAwesome5 } from "@expo/vector-icons";
 import { Audio } from "expo-av";
+import { openURL } from "expo-linking";
+import { Box, Button, HStack, Text, View } from "native-base";
+import { PixelRatio, useWindowDimensions } from "react-native";
+
+import DBRibbon from "../../../../../assets/svgs/DBRibbon";
 
 export const ExplorerItem = ({
-    resourceLink = "https://danceblue.org",
-    isText = false,
-    blogTitle = "",
-    blogContent = "",
-    isAudio = false,
-    isInstagram = false,
-    isTikTok = false,
-    isYouTube = false,
+  resourceLink = "https://danceblue.org",
+  isText = false,
+  blogTitle = "",
+  blogContent = "",
+  isAudio = false,
+  isInstagram = false,
+  isTikTok = false,
+  isYouTube = false,
 }: {
-    resourceLink: string;
-    isText: boolean;
-    blogTitle: string;
-    blogContent: string;
-    isAudio: boolean;
-    isInstagram: boolean;
-    isTikTok: boolean;
-    isYouTube: boolean;
+  resourceLink: string;
+  isText: boolean;
+  blogTitle: string;
+  blogContent: string;
+  isAudio: boolean;
+  isInstagram: boolean;
+  isTikTok: boolean;
+  isYouTube: boolean;
 }) => {
-    const { fontScale } = useWindowDimensions();
+  const { fontScale } = useWindowDimensions();
 
-    // DBLogoCondensed is currently broken
-    //let icon = <DBLogoCondensed svgProps={{ width: screenWidth * 0.12, height: screenWidth * 0.12 }} letterColor="#0032A0" ribbonColor="#FFC72C"/>;
-    let icon = <DBRibbon svgProps={{ width: PixelRatio.get() * 12, height: PixelRatio.get() * 12 }}/>;
+  // DBLogoCondensed is currently broken
+  // let icon = <DBLogoCondensed svgProps={{ width: screenWidth * 0.12, height: screenWidth * 0.12 }} letterColor="#0032A0" ribbonColor="#FFC72C"/>;
+  let icon = (
+    <DBRibbon
+      svgProps={{ width: PixelRatio.get() * 12, height: PixelRatio.get() * 12 }}
+    />
+  );
 
-    const headerFontSize = fontScale * 15;
-    const blogTitleFontSize = fontScale * 16;
-    const blogContentFontSize = fontScale * 14;
+  const headerFontSize = fontScale * 15;
+  const blogTitleFontSize = fontScale * 16;
+  const blogContentFontSize = fontScale * 14;
 
-    let source = "Our Imagination";
-    let link = "https://danceblue.org";
-    let content = 
-        <>
-            <Text fontSize={blogContentFontSize} textAlign="justify" fontFamily="">DanceBlue is an entirely student-run organization that fundraises year-round for the DanceBlue Hematology/Oncology Clinic and culminates in a 24-hour no sitting, no sleeping dance marathon.</Text>
-        </>;
+  let source = "Our Imagination";
+  let link = "https://danceblue.org";
+  let content = (
+    <>
+      <Text fontSize={blogContentFontSize} textAlign="justify" fontFamily="">
+        DanceBlue is an entirely student-run organization that fundraises
+        year-round for the DanceBlue Hematology/Oncology Clinic and culminates
+        in a 24-hour no sitting, no sleeping dance marathon.
+      </Text>
+    </>
+  );
 
-    if (isText) {
-        icon = <FontAwesome5 name="compass" size={PixelRatio.get() * 9} color="#0032A0"/>
-        source = "DB Blog";
-        link = "https://danceblue.org/news";
-        
-        content = 
-            <View width="100%">
-                <Text textAlign="center" fontSize={blogTitleFontSize}>{blogTitle}</Text>
-                <Text fontSize={blogContentFontSize} textAlign="justify" fontFamily=""> { blogContent } </Text>
-                <Box width="full" alignItems="flex-end">
-                    <Button marginTop={0.5} width="1/3" onPress={() => { openURL(resourceLink).catch(universalCatch) }}>Read More!</Button>
-                </Box>
-            </View>;
-
-    } else if (isAudio) {
-        icon = <FontAwesome5 name="music" size={PixelRatio.get() * 9} color="#0032A0"/>
-        source = "DB Podcast";
-        link = "https://danceblue.org/category/podcast";
-
-        Audio.setAudioModeAsync({
-            staysActiveInBackground: true,
-            playsInSilentModeIOS: true,
-        }).catch(showMessage);
-
-        const sound = new Audio.Sound();
-        sound.loadAsync({ uri: resourceLink });
-
-        content = 
-            <>
-                <View>
-                    <Text textAlign="center" fontSize={blogTitleFontSize}>{blogTitle}</Text>
-                    <AudioPlayer
-                        sound={sound}
-                        loading={!sound}
-                        title=""
-                        titleLink="https://danceblue.org/category/podcast/"
-                    />
-                </View>
-            </>;
-    } else if (isInstagram) {
-        icon = <FontAwesome5 name="instagram" size={PixelRatio.get() * 9} color="#0032A0"/>
-        source = "Instagram";
-        link = "https://instagram.com/uk_danceblue";
-        content = 
-            <>
-                <Text>Instagram</Text>
-            </>;
-    } else if (isTikTok) {
-        icon = <FontAwesome5 name="tiktok" size={PixelRatio.get() * 9} color="#0032A0"/>
-        source = "TikTok";
-        link = "https://tiktok.com/@uk_danceblue";
-        content = 
-            <>
-                <Text>TikTok</Text>
-            </>;
-    } else if (isYouTube) {
-        icon = <FontAwesome5 name="youtube" size={PixelRatio.get() * 9} color="#0032A0"/>
-        source = "YouTube";
-        link = "https://www.youtube.com/channel/UCcF8V41xkzYkZ0B1IOXntjg";
-        content = 
-            <>
-                <Text>YouTube</Text>
-            </>;
-    }
-
-    return (
-        <View /* borderBottomColor="#c5c6d0" borderBottomWidth={0.5} */ marginTop={5}>
-            { /* THIS IS THE HEADER ROW */ }
-            <View borderBottomColor="#c5c6d0" borderBottomWidth={0.5}>
-                <HStack alignItems="center" marginLeft={2} marginY={2}>
-                    {icon}
-                    <Text marginLeft={2} fontSize={headerFontSize} onPress={() => { openURL(link).catch(universalCatch) }}>{source}</Text>
-                </HStack>
-            </View>
-
-            { /* THIS IS THE CONTENT ROW */ }
-            <HStack margin={2}>
-                {content}
-            </HStack>
-        </View>
+  if (isText) {
+    icon = (
+      <FontAwesome5
+        name="compass"
+        size={PixelRatio.get() * 9}
+        color="#0032A0"
+      />
     );
+    source = "DB Blog";
+    link = "https://danceblue.org/news";
+
+    content = (
+      <View width="100%">
+        <Text textAlign="center" fontSize={blogTitleFontSize}>
+          {blogTitle}
+        </Text>
+        <Text fontSize={blogContentFontSize} textAlign="justify" fontFamily="">
+          {" "}
+          {blogContent}{" "}
+        </Text>
+        <Box width="full" alignItems="flex-end">
+          <Button
+            marginTop={0.5}
+            width="1/3"
+            onPress={() => {
+              openURL(resourceLink).catch(universalCatch);
+            }}
+          >
+            Read More!
+          </Button>
+        </Box>
+      </View>
+    );
+  } else if (isAudio) {
+    icon = (
+      <FontAwesome5 name="music" size={PixelRatio.get() * 9} color="#0032A0" />
+    );
+    source = "DB Podcast";
+    link = "https://danceblue.org/category/podcast";
+
+    Audio.setAudioModeAsync({
+      staysActiveInBackground: true,
+      playsInSilentModeIOS: true,
+    }).catch(showMessage);
+
+    const sound = new Audio.Sound();
+    sound.loadAsync({ uri: resourceLink });
+
+    content = (
+      <>
+        <View>
+          <Text textAlign="center" fontSize={blogTitleFontSize}>
+            {blogTitle}
+          </Text>
+          <AudioPlayer
+            sound={sound}
+            loading={!sound}
+            title=""
+            titleLink="https://danceblue.org/category/podcast/"
+          />
+        </View>
+      </>
+    );
+  } else if (isInstagram) {
+    icon = (
+      <FontAwesome5
+        name="instagram"
+        size={PixelRatio.get() * 9}
+        color="#0032A0"
+      />
+    );
+    source = "Instagram";
+    link = "https://instagram.com/uk_danceblue";
+    content = (
+      <>
+        <Text>Instagram</Text>
+      </>
+    );
+  } else if (isTikTok) {
+    icon = (
+      <FontAwesome5 name="tiktok" size={PixelRatio.get() * 9} color="#0032A0" />
+    );
+    source = "TikTok";
+    link = "https://tiktok.com/@uk_danceblue";
+    content = (
+      <>
+        <Text>TikTok</Text>
+      </>
+    );
+  } else if (isYouTube) {
+    icon = (
+      <FontAwesome5
+        name="youtube"
+        size={PixelRatio.get() * 9}
+        color="#0032A0"
+      />
+    );
+    source = "YouTube";
+    link = "https://www.youtube.com/channel/UCcF8V41xkzYkZ0B1IOXntjg";
+    content = (
+      <>
+        <Text>YouTube</Text>
+      </>
+    );
+  }
+
+  return (
+    <View
+      /* borderBottomColor="#c5c6d0" borderBottomWidth={0.5} */ marginTop={5}
+    >
+      {/* THIS IS THE HEADER ROW */}
+      <View borderBottomColor="#c5c6d0" borderBottomWidth={0.5}>
+        <HStack alignItems="center" marginLeft={2} marginY={2}>
+          {icon}
+          <Text
+            marginLeft={2}
+            fontSize={headerFontSize}
+            onPress={() => {
+              openURL(link).catch(universalCatch);
+            }}
+          >
+            {source}
+          </Text>
+        </HStack>
+      </View>
+
+      {/* THIS IS THE CONTENT ROW */}
+      <HStack margin={2}>{content}</HStack>
+    </View>
+  );
 };
--- packages/mobile/src/navigation/root/tab/ExplorerScreen/ExplorerScreen.tsx
+++ packages/mobile/src/navigation/root/tab/ExplorerScreen/ExplorerScreen.tsx
@@ -1,50 +1,50 @@
-import { VStack, ScrollView } from "native-base";
-import { ExplorerItem } from "./ExplorerItem";
 import { useNetworkStatus } from "@common/customHooks";
+import { ScrollView, VStack } from "native-base";
 import { useState } from "react";
-import { FeedItem, parse } from "react-native-rss-parser";
-import { DateTime } from "luxon";
+import type { FeedItem } from "react-native-rss-parser";
+import { parse } from "react-native-rss-parser";
+
+import { ExplorerItem } from "./ExplorerItem";
 
 export const ExplorerScreen = () => {
-    const [{ isConnected }, isNetStatusLoaded] = useNetworkStatus();
-    const [blogPosts, setBlogPosts] = useState<FeedItem[] | undefined>();
-    const [podcasts, setPodcasts] = useState<FeedItem[] | undefined>();
-    const [instagramPosts, setInstagramPosts] = useState<undefined>();
-    const [tiktokPosts, setTikTokPosts] = useState<undefined>();
-    const [youtubePosts, setYoutubePosts] = useState<undefined>();
+  const [{ isConnected }, isNetStatusLoaded] = useNetworkStatus();
+  const [blogPosts, setBlogPosts] = useState<FeedItem[] | undefined>();
+  const [podcasts, setPodcasts] = useState<FeedItem[] | undefined>();
+  const [instagramPosts, setInstagramPosts] = useState();
+  const [tiktokPosts, setTikTokPosts] = useState();
+  const [youtubePosts, setYoutubePosts] = useState();
 
-    const loadFeed = async () => {
-        try {
-            const dbWebsiteRSS = await fetch("https://danceblue.org/feed");
-            const dbWebsiteXML = await dbWebsiteRSS.text();
-            const dbWebsiteParsed = await parse(dbWebsiteXML);
+  const loadFeed = async () => {
+    try {
+      const dbWebsiteRSS = await fetch("https://danceblue.org/feed");
+      const dbWebsiteXML = await dbWebsiteRSS.text();
+      const dbWebsiteParsed = await parse(dbWebsiteXML);
 
-            const blogPosts = dbWebsiteParsed.items
-                .filter((item) =>
-                    item.categories.some((category) => category?.name !== "Podcast")
-                );
+      const blogPosts = dbWebsiteParsed.items.filter((item) =>
+        item.categories.some((category) => category?.name !== "Podcast")
+      );
 
-            setBlogPosts(blogPosts);
+      setBlogPosts(blogPosts);
 
-            const podcastPosts = dbWebsiteParsed.items
-                .filter((item) =>
-                    item.categories.some((category) => category?.name === "Podcast")
-                )
-                .filter((item) =>
-                    item.enclosures.some(
-                        (enclosure) => enclosure.mimeType === "audio/mpeg"
-                    )
-                );
-                
-            setPodcasts(podcastPosts);
-        } catch (error) {
-            console.error(error);
-        }
+      const podcastPosts = dbWebsiteParsed.items
+        .filter((item) =>
+          item.categories.some((category) => category?.name === "Podcast")
+        )
+        .filter((item) =>
+          item.enclosures.some(
+            (enclosure) => enclosure.mimeType === "audio/mpeg"
+          )
+        );
 
-        // TODO: Implement Instagram, TikTok, YouTube RSS if possible
-    };
+      setPodcasts(podcastPosts);
+    } catch (error) {
+      console.error(error);
+    }
 
-    /*
+    // TODO: Implement Instagram, TikTok, YouTube RSS if possible
+  };
+
+  /*
         TO SORT FeedItem[]      (blog posts and podcasts)
 
             .map((item) => ({
@@ -60,12 +60,12 @@ export const ExplorerScreen = () => {
             );
     */
 
-    loadFeed();
-    
-    /*
+  loadFeed();
+
+  /*
 
         TODO:
-        
+
         1. Iterate through all posts types
             - Add them to a map of:
                 * <ExplorerItem>
@@ -75,30 +75,72 @@ export const ExplorerScreen = () => {
             - Add the item to the return component
     */
 
-    /*
-     * Called by React Native when rendering the screen
-     */
-    return (
-        <VStack h="full">
-            <ScrollView flex={1}>
-
-                <ExplorerItem isText={false} resourceLink={""} blogTitle={""} blogContent={""} isAudio={false} isInstagram={false} isTikTok={false} isYouTube={false}/>
-                <ExplorerItem 
-                    isText={true} resourceLink={""} 
-                    blogTitle={"Celebrating 25 Years of Jarret's Joy Cart"} 
-                    blogContent={"25 years ago, despite six cancer diagnoses and an amputated leg, Jarrett Mynear, the inspiration for DanceBlue, began his mission to spread joy in the world one toy at a time. Refusing to let his illness hold him back and seeing the need for a spark in the Kentucky Children’s Oncology Clinic he was treated in, Jarrett began his Joy Cart. It started small, [...]"} 
-                    isAudio={false} isInstagram={false} isTikTok={false} isYouTube={false}/>
-                {/* 
-                    <ExplorerItem 
-                        isText={false} 
-                        resourceLink={"https://danceblue.org/wp-content/uploads/2024/01/DBPodcast1.mp3"} 
+  /*
+   * Called by React Native when rendering the screen
+   */
+  return (
+    <VStack h="full">
+      <ScrollView flex={1}>
+        <ExplorerItem
+          isText={false}
+          resourceLink={""}
+          blogTitle={""}
+          blogContent={""}
+          isAudio={false}
+          isInstagram={false}
+          isTikTok={false}
+          isYouTube={false}
+        />
+        <ExplorerItem
+          isText={true}
+          resourceLink={""}
+          blogTitle={"Celebrating 25 Years of Jarret's Joy Cart"}
+          blogContent={
+            "25 years ago, despite six cancer diagnoses and an amputated leg, Jarrett Mynear, the inspiration for DanceBlue, began his mission to spread joy in the world one toy at a time. Refusing to let his illness hold him back and seeing the need for a spark in the Kentucky Children’s Oncology Clinic he was treated in, Jarrett began his Joy Cart. It started small, [...]"
+          }
+          isAudio={false}
+          isInstagram={false}
+          isTikTok={false}
+          isYouTube={false}
+        />
+        {/*
+                    <ExplorerItem
+                        isText={false}
+                        resourceLink={"https://danceblue.org/wp-content/uploads/2024/01/DBPodcast1.mp3"}
                         blogTitle={"Podcast- DB Behind the Scenes"} blogContent={""}
                         isAudio={true} isInstagram={false} isTikTok={false} isYouTube={false}/>
                 */}
-                <ExplorerItem isText={false} resourceLink={""} blogTitle={""} blogContent={""} isAudio={false} isInstagram={true} isTikTok={false} isYouTube={false}/>
-                <ExplorerItem isText={false} resourceLink={""} blogTitle={""} blogContent={""} isAudio={false} isInstagram={false} isTikTok={true} isYouTube={false}/>
-                <ExplorerItem isText={false} resourceLink={""} blogTitle={""} blogContent={""} isAudio={false} isInstagram={false} isTikTok={false} isYouTube={true}/>
-            </ScrollView>
-        </VStack>
-    );
+        <ExplorerItem
+          isText={false}
+          resourceLink={""}
+          blogTitle={""}
+          blogContent={""}
+          isAudio={false}
+          isInstagram={true}
+          isTikTok={false}
+          isYouTube={false}
+        />
+        <ExplorerItem
+          isText={false}
+          resourceLink={""}
+          blogTitle={""}
+          blogContent={""}
+          isAudio={false}
+          isInstagram={false}
+          isTikTok={true}
+          isYouTube={false}
+        />
+        <ExplorerItem
+          isText={false}
+          resourceLink={""}
+          blogTitle={""}
+          blogContent={""}
+          isAudio={false}
+          isInstagram={false}
+          isTikTok={false}
+          isYouTube={true}
+        />
+      </ScrollView>
+    </VStack>
+  );
 };
