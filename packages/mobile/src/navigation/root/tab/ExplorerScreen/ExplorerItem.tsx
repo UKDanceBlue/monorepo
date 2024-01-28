8451f18322698875c006878b8ef0eeb13b349bb2
Jacksonnnnn <jackson.huse@stu.jefferson.kyschools.us>
2024-01-28 13:57:13 -0500

    Created ExplorerItem

---
+++ packages/mobile/src/navigation/root/tab/ExplorerScreen/ExplorerItem.tsx
@@ -0,0 +1,7 @@
+import { Text } from "native-base";
+
+export const ExplorerItem = () => {
+    return (
+        <Text>blah</Text>
+    );
+};
--- packages/mobile/src/navigation/root/tab/ExplorerScreen/ExplorerScreen.tsx
+++ packages/mobile/src/navigation/root/tab/ExplorerScreen/ExplorerScreen.tsx
@@ -1,11 +1,12 @@
+import { ExplorerItem } from "./ExplorerItem";
+
 export const ExplorerScreen = () => {
-    
-  
     /*
      * Called by React Native when rendering the screen
      */
     return (
         <>
+        <ExplorerItem/>
         </>
     );
 };
