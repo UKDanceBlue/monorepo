d57d871df246e286f1d1452c6199b148ed6db536
Jacksonnnnn <jackson.huse@stu.jefferson.kyschools.us>
2024-01-28 16:13:25 -0500

    Added blog and podcast feeds, planned out todos

--- packages/mobile/src/navigation/root/tab/ExplorerScreen/ExplorerScreen.tsx
+++ packages/mobile/src/navigation/root/tab/ExplorerScreen/ExplorerScreen.tsx
@@ -1,13 +1,87 @@
 import { VStack, ScrollView } from "native-base";
 import { ExplorerItem } from "./ExplorerItem";
+import { useNetworkStatus } from "@common/customHooks";
+import { useState } from "react";
+import { FeedItem, parse } from "react-native-rss-parser";
+import { DateTime } from "luxon";
 
 export const ExplorerScreen = () => {
+    const [{ isConnected }, isNetStatusLoaded] = useNetworkStatus();
+    const [blogPosts, setBlogPosts] = useState<FeedItem[] | undefined>();
+    const [podcasts, setPodcasts] = useState<FeedItem[] | undefined>();
+    const [instagramPosts, setInstagramPosts] = useState<undefined>();
+    const [tiktokPosts, setTikTokPosts] = useState<undefined>();
+    const [youtubePosts, setYoutubePosts] = useState<undefined>();
+
+    const loadFeed = async () => {
+        try {
+            const dbWebsiteRSS = await fetch("https://danceblue.org/feed");
+            const dbWebsiteXML = await dbWebsiteRSS.text();
+            const dbWebsiteParsed = await parse(dbWebsiteXML);
+
+            const blogPosts = dbWebsiteParsed.items
+                .filter((item) =>
+                    item.categories.some((category) => category?.name !== "Podcast")
+                );
+
+            setBlogPosts(blogPosts);
+
+            const podcastPosts = dbWebsiteParsed.items
+                .filter((item) =>
+                    item.categories.some((category) => category?.name === "Podcast")
+                )
+                .filter((item) =>
+                    item.enclosures.some(
+                        (enclosure) => enclosure.mimeType === "audio/mpeg"
+                    )
+                );
+                
+            setPodcasts(podcastPosts);
+        } catch (error) {
+            console.error(error);
+        }
+
+        // TODO: Implement Instagram, TikTok, YouTube RSS if possible
+    };
+
+    /*
+        TO SORT FeedItem[]      (blog posts and podcasts)
+
+            .map((item) => ({
+                ...item,
+                dateTimePublished: DateTime.fromRFC2822(item.published),
+            }))
+            .sort((a,b) =>
+                a.dateTimePublished > b.dateTimePublished
+                ? -1
+                : a.dateTimePublished > b.dateTimePublished
+                ? 1
+                : 0
+            );
+    */
+
+    loadFeed();
+    
+    /*
+
+        TODO:
+        
+        1. Iterate through all posts types
+            - Add them to a map of:
+                * <ExplorerItem>
+                * dateTimePublished
+        2. Sort the master list by their dateTimePublished
+        3. Iterate through each map item
+            - Add the item to the return component
+    */
+
     /*
      * Called by React Native when rendering the screen
      */
     return (
         <VStack h="full">
             <ScrollView flex={1}>
+
                 <ExplorerItem isText={false} resourceLink={""} blogTitle={""} blogContent={""} isAudio={false} isInstagram={false} isTikTok={false} isYouTube={false}/>
                 <ExplorerItem 
                     isText={true} resourceLink={""} 
