3d779866f4e9208aaa009579449e2719574f9309
Jacksonnnnn <jackson.huse@stu.jefferson.kyschools.us>
2024-01-29 19:04:59 -0500

    Fixed date issues

--- packages/mobile/src/navigation/root/tab/ExplorerScreen/ExplorerScreen.tsx
+++ packages/mobile/src/navigation/root/tab/ExplorerScreen/ExplorerScreen.tsx
@@ -24,63 +24,6 @@ export const ExplorerScreen = () => {
             item.jsxElement
           )
         }
-        <ExplorerItem
-          isText={false}
-          resourceLink={""}
-          blogTitle={""}
-          blogContent={""}
-          isAudio={false}
-          isInstagram={false}
-          isTikTok={false}
-          isYouTube={false}
-        />
-        <ExplorerItem
-          isText={true}
-          resourceLink={""}
-          blogTitle={"Celebrating 25 Years of Jarret's Joy Cart"}
-          blogContent={
-            "25 years ago, despite six cancer diagnoses and an amputated leg, Jarrett Mynear, the inspiration for DanceBlue, began his mission to spread joy in the world one toy at a time. Refusing to let his illness hold him back and seeing the need for a spark in the Kentucky Children’s Oncology Clinic he was treated in, Jarrett began his Joy Cart. It started small, [...]"
-          }
-          isAudio={false}
-          isInstagram={false}
-          isTikTok={false}
-          isYouTube={false}
-        />
-                    <ExplorerItem
-                        isText={false}
-                        resourceLink={"https://danceblue.org/wp-content/uploads/2024/01/DBPodcast1.mp3"}
-                        blogTitle={"Podcast- DB Behind the Scenes"} blogContent={""}
-                        isAudio={true} isInstagram={false} isTikTok={false} isYouTube={false}/>
-        <ExplorerItem
-          isText={false}
-          resourceLink={""}
-          blogTitle={""}
-          blogContent={""}
-          isAudio={false}
-          isInstagram={true}
-          isTikTok={false}
-          isYouTube={false}
-        />
-        <ExplorerItem
-          isText={false}
-          resourceLink={""}
-          blogTitle={""}
-          blogContent={""}
-          isAudio={false}
-          isInstagram={false}
-          isTikTok={true}
-          isYouTube={false}
-        />
-        <ExplorerItem
-          isText={false}
-          resourceLink={"https://www.youtube.com/embed/uDrXtr6pEVc"}
-          blogTitle={""}
-          blogContent={""}
-          isAudio={false}
-          isInstagram={false}
-          isTikTok={false}
-          isYouTube={true}
-        />
       </ScrollView>
     </VStack>
   );
