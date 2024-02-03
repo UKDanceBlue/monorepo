a61ea2c272d0fc90ef24ede96a1b0dfca98caf9a
Joshua Tag Howard <tag@jthoward.dev>
2024-02-03 14:54:04 -0500

    Add alias for type-graphql in test configuration

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -3,5 +3,10 @@ import { defineWorkspace } from "vitest/config";
 export default defineWorkspace([
   {
     root: "packages/common/lib",
+    test: {
+      alias: {
+        "type-graphql": "type-graphql/browser-shim.js",
+      },
+    },
   },
 ]);
