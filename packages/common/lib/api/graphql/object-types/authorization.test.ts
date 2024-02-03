118574f5b2f3e51334a37189ea82e3f4d19c0e1a
Joshua Tag Howard <tag@jthoward.dev>
2024-02-03 14:53:14 -0500

    Add role tests for access level and authorization

---
+++ packages/common/lib/api/graphql/object-types/authorization.test.ts
@@ -0,0 +1,185 @@
+import { describe } from "vitest";
+
+import type { Authorization } from "../../../index.js";
+import {
+  AccessLevel,
+  CommitteeIdentifier,
+  CommitteeRole,
+  DbRole,
+} from "../../../index.js";
+
+import { checkAuthorization } from "./authorization.js";
+
+const techChair: Authorization = {
+  accessLevel: AccessLevel.Admin,
+  dbRole: DbRole.Committee,
+  committeeIdentifier: CommitteeIdentifier.techCommittee,
+  committeeRole: CommitteeRole.Chair,
+};
+
+const techCoordinator: Authorization = {
+  accessLevel: AccessLevel.Admin,
+  dbRole: DbRole.Committee,
+  committeeIdentifier: CommitteeIdentifier.techCommittee,
+  committeeRole: CommitteeRole.Coordinator,
+};
+
+const techMember: Authorization = {
+  accessLevel: AccessLevel.Admin,
+  dbRole: DbRole.Committee,
+  committeeIdentifier: CommitteeIdentifier.techCommittee,
+  committeeRole: CommitteeRole.Member,
+};
+
+const overallChair: Authorization = {
+  accessLevel: AccessLevel.CommitteeChairOrCoordinator,
+  dbRole: DbRole.Committee,
+  committeeIdentifier: CommitteeIdentifier.viceCommittee,
+  committeeRole: CommitteeRole.Chair,
+};
+
+const dancerRelationsChair: Authorization = {
+  accessLevel: AccessLevel.CommitteeChairOrCoordinator,
+  dbRole: DbRole.Committee,
+  committeeIdentifier: CommitteeIdentifier.dancerRelationsCommittee,
+  committeeRole: CommitteeRole.Chair,
+};
+
+const dancerRelationsCoordinator: Authorization = {
+  accessLevel: AccessLevel.CommitteeChairOrCoordinator,
+  dbRole: DbRole.Committee,
+  committeeIdentifier: CommitteeIdentifier.dancerRelationsCommittee,
+  committeeRole: CommitteeRole.Coordinator,
+};
+
+const dancerRelationsMember: Authorization = {
+  accessLevel: AccessLevel.Committee,
+  dbRole: DbRole.Committee,
+  committeeIdentifier: CommitteeIdentifier.dancerRelationsCommittee,
+  committeeRole: CommitteeRole.Member,
+};
+
+const captain: Authorization = {
+  accessLevel: AccessLevel.TeamCaptain,
+  dbRole: DbRole.TeamCaptain,
+};
+
+const member: Authorization = {
+  accessLevel: AccessLevel.TeamMember,
+  dbRole: DbRole.TeamMember,
+};
+
+const publicAuth: Authorization = {
+  accessLevel: AccessLevel.Public,
+  dbRole: DbRole.Public,
+};
+
+const none: Authorization = {
+  accessLevel: AccessLevel.None,
+  dbRole: DbRole.None,
+};
+
+describe(checkAuthorization, (it) => {
+  it("should return true when the user's access level matches the required access level", (assert) => {
+    assert
+      .expect(checkAuthorization({ accessLevel: AccessLevel.Admin }, techChair))
+      .toBe(true);
+    assert
+      .expect(
+        checkAuthorization({ accessLevel: AccessLevel.Admin }, techCoordinator)
+      )
+      .toBe(true);
+    assert
+      .expect(
+        checkAuthorization({ accessLevel: AccessLevel.Admin }, techMember)
+      )
+      .toBe(true);
+    assert
+      .expect(
+        checkAuthorization(
+          { accessLevel: AccessLevel.CommitteeChairOrCoordinator },
+          overallChair
+        )
+      )
+      .toBe(true);
+    assert
+      .expect(
+        checkAuthorization(
+          { accessLevel: AccessLevel.CommitteeChairOrCoordinator },
+          dancerRelationsChair
+        )
+      )
+      .toBe(true);
+    assert
+      .expect(
+        checkAuthorization(
+          { accessLevel: AccessLevel.CommitteeChairOrCoordinator },
+          dancerRelationsCoordinator
+        )
+      )
+      .toBe(true);
+    assert
+      .expect(
+        checkAuthorization(
+          { accessLevel: AccessLevel.Committee },
+          dancerRelationsMember
+        )
+      )
+      .toBe(true);
+    assert
+      .expect(
+        checkAuthorization({ accessLevel: AccessLevel.TeamCaptain }, captain)
+      )
+      .toBe(true);
+    assert
+      .expect(
+        checkAuthorization({ accessLevel: AccessLevel.TeamMember }, member)
+      )
+      .toBe(true);
+    assert
+      .expect(
+        checkAuthorization({ accessLevel: AccessLevel.Public }, publicAuth)
+      )
+      .toBe(true);
+    assert
+      .expect(checkAuthorization({ accessLevel: AccessLevel.None }, none))
+      .toBe(true);
+  });
+
+  it("should return true when the user's access level is higher than the required access level", (assert) => {
+    assert
+      .expect(
+        checkAuthorization({ accessLevel: AccessLevel.Committee }, techChair)
+      )
+      .toBe(true);
+    assert
+      .expect(
+        checkAuthorization(
+          { accessLevel: AccessLevel.Committee },
+          techCoordinator
+        )
+      )
+      .toBe(true);
+    assert
+      .expect(
+        checkAuthorization({ accessLevel: AccessLevel.Committee }, techMember)
+      )
+      .toBe(true);
+    assert
+      .expect(
+        checkAuthorization({ accessLevel: AccessLevel.TeamCaptain }, captain)
+      )
+      .toBe(true);
+    assert
+      .expect(
+        checkAuthorization({ accessLevel: AccessLevel.TeamMember }, captain)
+      )
+      .toBe(true);
+    assert
+      .expect(checkAuthorization({ accessLevel: AccessLevel.Public }, captain))
+      .toBe(true);
+    assert
+      .expect(checkAuthorization({ accessLevel: AccessLevel.None }, captain))
+      .toBe(true);
+  });
+});
---
+++ packages/common/lib/auth/role.test.ts
@@ -0,0 +1,130 @@
+import { describe, expect, it } from "vitest";
+
+import {
+  AccessLevel,
+  CommitteeIdentifier,
+  CommitteeRole,
+  DbRole,
+  RoleResource,
+} from "../index.js";
+
+import { roleToAccessLevel, roleToAuthorization } from "./role.js";
+
+describe(roleToAccessLevel, () => {
+  it("returns the correct access level for a given role normally", () => {
+    const chairRole = RoleResource.init({
+      dbRole: DbRole.Committee,
+      committeeRole: CommitteeRole.Chair,
+      committeeIdentifier: CommitteeIdentifier.dancerRelationsCommittee,
+    });
+    expect(roleToAccessLevel(chairRole)).toBe(
+      AccessLevel.CommitteeChairOrCoordinator
+    );
+
+    const coordinatorRole = RoleResource.init({
+      dbRole: DbRole.Committee,
+      committeeRole: CommitteeRole.Coordinator,
+      committeeIdentifier: CommitteeIdentifier.dancerRelationsCommittee,
+    });
+    expect(roleToAccessLevel(coordinatorRole)).toBe(
+      AccessLevel.CommitteeChairOrCoordinator
+    );
+
+    const memberRole = RoleResource.init({
+      dbRole: DbRole.Committee,
+      committeeRole: CommitteeRole.Member,
+      committeeIdentifier: CommitteeIdentifier.dancerRelationsCommittee,
+    });
+    expect(roleToAccessLevel(memberRole)).toBe(AccessLevel.Committee);
+
+    const teamCaptainRole = RoleResource.init({
+      dbRole: DbRole.TeamCaptain,
+    });
+    expect(roleToAccessLevel(teamCaptainRole)).toBe(AccessLevel.TeamCaptain);
+
+    const teamMemberRole = RoleResource.init({
+      dbRole: DbRole.TeamMember,
+    });
+    expect(roleToAccessLevel(teamMemberRole)).toBe(AccessLevel.TeamMember);
+
+    const publicRole = RoleResource.init({
+      dbRole: DbRole.Public,
+    });
+    expect(roleToAccessLevel(publicRole)).toBe(AccessLevel.Public);
+
+    const noneRole = RoleResource.init({
+      dbRole: DbRole.None,
+    });
+    expect(roleToAccessLevel(noneRole)).toBe(AccessLevel.None);
+  });
+
+  it("grants any member of the tech committee admin access", () => {
+    const chairRole = RoleResource.init({
+      dbRole: DbRole.Committee,
+      committeeRole: CommitteeRole.Chair,
+      committeeIdentifier: CommitteeIdentifier.techCommittee,
+    });
+    expect(roleToAccessLevel(chairRole)).toBe(AccessLevel.Admin);
+
+    const coordinatorRole = RoleResource.init({
+      dbRole: DbRole.Committee,
+      committeeRole: CommitteeRole.Coordinator,
+      committeeIdentifier: CommitteeIdentifier.techCommittee,
+    });
+    expect(roleToAccessLevel(coordinatorRole)).toBe(AccessLevel.Admin);
+
+    const memberRole = RoleResource.init({
+      dbRole: DbRole.Committee,
+      committeeRole: CommitteeRole.Member,
+      committeeIdentifier: CommitteeIdentifier.techCommittee,
+    });
+    expect(roleToAccessLevel(memberRole)).toBe(AccessLevel.Admin);
+  });
+
+  it("throws an error for an illegal role", () => {
+    const illegalRole = RoleResource.init({
+      dbRole: "illegal" as DbRole,
+    });
+    expect(() => roleToAccessLevel(illegalRole)).toThrowError(
+      "Illegal DbRole: [Parsing of 'illegal' failed]"
+    );
+  });
+});
+
+describe(roleToAuthorization, () => {
+  it("converts a role to an authorization object", () => {
+    const role = RoleResource.init({
+      dbRole: DbRole.TeamMember,
+    });
+    expect(roleToAuthorization(role)).toEqual({
+      dbRole: DbRole.TeamMember,
+      accessLevel: AccessLevel.TeamMember,
+    });
+  });
+
+  it("converts a role with a committee set to an authorization object", () => {
+    const role = RoleResource.init({
+      dbRole: DbRole.Committee,
+      committeeRole: CommitteeRole.Member,
+      committeeIdentifier: CommitteeIdentifier.dancerRelationsCommittee,
+    });
+    expect(roleToAuthorization(role)).toEqual({
+      dbRole: DbRole.Committee,
+      accessLevel: AccessLevel.Committee,
+      committeeRole: CommitteeRole.Member,
+      committeeIdentifier: CommitteeIdentifier.dancerRelationsCommittee,
+    });
+  });
+
+  it("throws an error if one of committee or committeeRole is set without the other", ({
+    expect,
+  }) => {
+    const role = RoleResource.init({
+      dbRole: DbRole.Committee,
+      committeeRole: CommitteeRole.Chair,
+    });
+    expect(() => roleToAuthorization(role)).toThrowError(
+      "Cannot have a committee role without a committee or vice versa"
+    );
+  });
+});
