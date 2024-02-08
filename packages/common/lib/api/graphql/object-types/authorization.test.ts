ff7403593bc979c347f8169c9f3224c71861aa1e
Joshua Tag Howard <tag@jthoward.dev>
2024-02-08 13:12:38 -0500

    Update user authentication logic

--- packages/common/lib/api/graphql/object-types/authorization.test.ts
+++ packages/common/lib/api/graphql/object-types/authorization.test.ts
@@ -1,4 +1,4 @@
-import { describe } from "vitest";
+import { describe, expect } from "vitest";
 
 import type { Authorization } from "../../../index.js";
 import {
@@ -80,143 +80,299 @@ const none: Authorization = {
 };
 
 describe(checkAuthorization, (it) => {
-  it("should return true when the user's access level matches the required access level", (assert) => {
-    assert
-      .expect(checkAuthorization({ accessLevel: AccessLevel.Admin }, techChair))
-      .toBe(true);
-    assert
-      .expect(
-        checkAuthorization({ accessLevel: AccessLevel.Admin }, techCoordinator)
+  it("should return true when the user's access level matches the required access level", () => {
+    expect(
+      checkAuthorization({ accessLevel: AccessLevel.Admin }, techChair)
+    ).toBe(true);
+    expect(
+      checkAuthorization({ accessLevel: AccessLevel.Admin }, techCoordinator)
+    ).toBe(true);
+    expect(
+      checkAuthorization({ accessLevel: AccessLevel.Admin }, techMember)
+    ).toBe(true);
+    expect(
+      checkAuthorization(
+        { accessLevel: AccessLevel.CommitteeChairOrCoordinator },
+        overallChair
       )
-      .toBe(true);
-    assert
-      .expect(
-        checkAuthorization({ accessLevel: AccessLevel.Admin }, techMember)
+    ).toBe(true);
+    expect(
+      checkAuthorization(
+        { accessLevel: AccessLevel.CommitteeChairOrCoordinator },
+        dancerRelationsChair
       )
-      .toBe(true);
-    assert
-      .expect(
-        checkAuthorization(
-          { accessLevel: AccessLevel.CommitteeChairOrCoordinator },
-          overallChair
-        )
+    ).toBe(true);
+    expect(
+      checkAuthorization(
+        { accessLevel: AccessLevel.CommitteeChairOrCoordinator },
+        dancerRelationsCoordinator
       )
-      .toBe(true);
-    assert
-      .expect(
-        checkAuthorization(
-          { accessLevel: AccessLevel.CommitteeChairOrCoordinator },
-          dancerRelationsChair
-        )
+    ).toBe(true);
+    expect(
+      checkAuthorization(
+        { accessLevel: AccessLevel.Committee },
+        dancerRelationsMember
       )
-      .toBe(true);
-    assert
-      .expect(
-        checkAuthorization(
-          { accessLevel: AccessLevel.CommitteeChairOrCoordinator },
-          dancerRelationsCoordinator
-        )
-      )
-      .toBe(true);
-    assert
-      .expect(
-        checkAuthorization(
-          { accessLevel: AccessLevel.Committee },
-          dancerRelationsMember
-        )
-      )
-      .toBe(true);
-    assert
-      .expect(
-        checkAuthorization({ accessLevel: AccessLevel.TeamCaptain }, captain)
-      )
-      .toBe(true);
-    assert
-      .expect(
-        checkAuthorization({ accessLevel: AccessLevel.TeamMember }, member)
-      )
-      .toBe(true);
-    assert
-      .expect(
-        checkAuthorization({ accessLevel: AccessLevel.Public }, publicAuth)
-      )
-      .toBe(true);
-    assert
-      .expect(checkAuthorization({ accessLevel: AccessLevel.None }, none))
-      .toBe(true);
+    ).toBe(true);
+    expect(
+      checkAuthorization({ accessLevel: AccessLevel.TeamCaptain }, captain)
+    ).toBe(true);
+    expect(
+      checkAuthorization({ accessLevel: AccessLevel.TeamMember }, member)
+    ).toBe(true);
+    expect(
+      checkAuthorization({ accessLevel: AccessLevel.Public }, publicAuth)
+    ).toBe(true);
+    expect(checkAuthorization({ accessLevel: AccessLevel.None }, none)).toBe(
+      true
+    );
   });
 
-  it("should return true when the user's access level is higher than the required access level", (assert) => {
-    assert
-      .expect(
-        checkAuthorization({ accessLevel: AccessLevel.Committee }, techChair)
+  it("should return true when the user's access level is higher than the required access level", () => {
+    expect(
+      checkAuthorization({ accessLevel: AccessLevel.Committee }, techChair)
+    ).toBe(true);
+    expect(
+      checkAuthorization(
+        { accessLevel: AccessLevel.Committee },
+        techCoordinator
       )
-      .toBe(true);
-    assert
-      .expect(
-        checkAuthorization(
-          { accessLevel: AccessLevel.Committee },
-          techCoordinator
-        )
-      )
-      .toBe(true);
-    assert
-      .expect(
-        checkAuthorization({ accessLevel: AccessLevel.Committee }, techMember)
-      )
-      .toBe(true);
-    assert
-      .expect(
-        checkAuthorization({ accessLevel: AccessLevel.TeamCaptain }, captain)
-      )
-      .toBe(true);
-    assert
-      .expect(
-        checkAuthorization({ accessLevel: AccessLevel.TeamMember }, captain)
-      )
-      .toBe(true);
-    assert
-      .expect(checkAuthorization({ accessLevel: AccessLevel.Public }, captain))
-      .toBe(true);
-    assert
-      .expect(checkAuthorization({ accessLevel: AccessLevel.None }, captain))
-      .toBe(true);
+    ).toBe(true);
+    expect(
+      checkAuthorization({ accessLevel: AccessLevel.Committee }, techMember)
+    ).toBe(true);
+    expect(
+      checkAuthorization({ accessLevel: AccessLevel.TeamCaptain }, captain)
+    ).toBe(true);
+    expect(
+      checkAuthorization({ accessLevel: AccessLevel.TeamMember }, captain)
+    ).toBe(true);
+    expect(
+      checkAuthorization({ accessLevel: AccessLevel.Public }, captain)
+    ).toBe(true);
+    expect(checkAuthorization({ accessLevel: AccessLevel.None }, captain)).toBe(
+      true
+    );
   });
 
-  it("should return false when the user's access level is lower than the required access level", (assert) => {
-    assert
-      .expect(
-        checkAuthorization({ accessLevel: AccessLevel.Committee }, overallChair)
+  it("should return false when the user's access level is lower than the required access level", () => {
+    expect(
+      checkAuthorization(
+        { accessLevel: AccessLevel.CommitteeChairOrCoordinator },
+        dancerRelationsMember
       )
-      .toBe(false);
-    assert
-      .expect(
-        checkAuthorization(
-          { accessLevel: AccessLevel.Committee },
-          dancerRelationsChair
-        )
+    ).toBe(false);
+    expect(
+      checkAuthorization({ accessLevel: AccessLevel.Committee }, captain)
+    ).toBe(false);
+    expect(
+      checkAuthorization({ accessLevel: AccessLevel.TeamCaptain }, member)
+    ).toBe(false);
+    expect(
+      checkAuthorization({ accessLevel: AccessLevel.TeamMember }, publicAuth)
+    ).toBe(false);
+    expect(checkAuthorization({ accessLevel: AccessLevel.Public }, none)).toBe(
+      false
+    );
+    expect(
+      checkAuthorization(
+        { accessLevel: AccessLevel.CommitteeChairOrCoordinator },
+        none
       )
-      .toBe(false);
-    assert
-      .expect(
-        checkAuthorization(
-          { accessLevel: AccessLevel.Committee },
-          dancerRelationsCoordinator
-        )
+    ).toBe(false);
+  });
+
+  it("should work with a custom check function", () => {
+    expect(
+      checkAuthorization(
+        {
+          custom() {
+            return true;
+          },
+        },
+        { accessLevel: AccessLevel.None, dbRole: DbRole.None }
       )
-      .toBe(false);
-    assert
-      .expect(
-        checkAuthorization({ accessLevel: AccessLevel.TeamCaptain }, member)
+    ).toBe(true);
+    expect(
+      checkAuthorization(
+        {
+          custom() {
+            return false;
+          },
+        },
+        { accessLevel: AccessLevel.None, dbRole: DbRole.None }
       )
-      .toBe(false);
-    assert
-      .expect(
-        checkAuthorization({ accessLevel: AccessLevel.TeamMember }, publicAuth)
+    ).toBe(false);
+  });
+
+  it("should work with committeeIdentifier matching", () => {
+    expect(
+      checkAuthorization(
+        {
+          committeeIdentifier: CommitteeIdentifier.techCommittee,
+        },
+        techChair
       )
-      .toBe(false);
-    assert
-      .expect(checkAuthorization({ accessLevel: AccessLevel.Public }, none))
-      .toBe(false);
+    ).toBe(true);
+    expect(
+      checkAuthorization(
+        {
+          committeeIdentifier: CommitteeIdentifier.techCommittee,
+        },
+        none
+      )
+    ).toBe(false);
+  });
+
+  it("should work with committeeIdentifiers matching", () => {
+    expect(
+      checkAuthorization(
+        {
+          committeeIdentifiers: [
+            CommitteeIdentifier.techCommittee,
+            CommitteeIdentifier.viceCommittee,
+          ],
+        },
+        techChair
+      )
+    ).toBe(true);
+    expect(
+      checkAuthorization(
+        {
+          committeeIdentifiers: [
+            CommitteeIdentifier.techCommittee,
+            CommitteeIdentifier.viceCommittee,
+          ],
+        },
+        none
+      )
+    ).toBe(false);
+  });
+
+  it("should work with exact dbRole matching", () => {
+    expect(
+      checkAuthorization(
+        {
+          dbRole: DbRole.Committee,
+        },
+        techChair
+      )
+    ).toBe(true);
+    expect(
+      checkAuthorization(
+        {
+          dbRole: DbRole.Committee,
+        },
+        none
+      )
+    ).toBe(false);
+  });
+
+  it("should work with exact committeeRole matching", () => {
+    expect(
+      checkAuthorization(
+        {
+          committeeRole: CommitteeRole.Chair,
+        },
+        techChair
+      )
+    ).toBe(true);
+    expect(
+      checkAuthorization(
+        {
+          committeeRole: CommitteeRole.Chair,
+        },
+        none
+      )
+    ).toBe(false);
+  });
+
+  it("should work with minimum dbRole matching", () => {
+    expect(
+      checkAuthorization(
+        {
+          minDbRole: DbRole.Committee,
+        },
+        techChair
+      )
+    ).toBe(true);
+    expect(
+      checkAuthorization(
+        {
+          minDbRole: DbRole.TeamCaptain,
+        },
+        techChair
+      )
+    ).toBe(true);
+    expect(
+      checkAuthorization(
+        {
+          minDbRole: DbRole.TeamCaptain,
+        },
+        none
+      )
+    ).toBe(false);
+  });
+
+  it("should work with minimum committeeRole matching", () => {
+    expect(
+      checkAuthorization(
+        {
+          minCommitteeRole: CommitteeRole.Chair,
+        },
+        techChair
+      )
+    ).toBe(true);
+    expect(
+      checkAuthorization(
+        {
+          minCommitteeRole: CommitteeRole.Coordinator,
+        },
+        techChair
+      )
+    ).toBe(true);
+    expect(
+      checkAuthorization(
+        {
+          minCommitteeRole: CommitteeRole.Coordinator,
+        },
+        none
+      )
+    ).toBe(false);
+  });
+
+  it("should work with all of the above", () => {
+    expect(
+      checkAuthorization(
+        {
+          accessLevel: AccessLevel.Admin,
+          dbRole: DbRole.Committee,
+          committeeIdentifier: CommitteeIdentifier.techCommittee,
+          committeeRole: CommitteeRole.Chair,
+        },
+        techChair
+      )
+    ).toBe(true);
+    expect(
+      checkAuthorization(
+        {
+          accessLevel: AccessLevel.Admin,
+          committeeIdentifier: CommitteeIdentifier.techCommittee,
+          minDbRole: DbRole.Committee,
+          minCommitteeRole: CommitteeRole.Coordinator,
+        },
+        techChair
+      )
+    ).toBe(true);
+    expect(
+      checkAuthorization(
+        {
+          accessLevel: AccessLevel.Admin,
+          committeeIdentifier: CommitteeIdentifier.techCommittee,
+          minCommitteeRole: CommitteeRole.Chair,
+        },
+        none
+      )
+    ).toBe(false);
   });
 });
