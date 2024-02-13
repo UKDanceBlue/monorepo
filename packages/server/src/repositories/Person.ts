9ae657bd16efe408768b46782ff58bd7a2b1d1b2
Joshua Tag Howard <tag@jthoward.dev>
2024-02-13 07:13:14 +0000

    Refactor PersonRepository class to support advanced filtering and sorting

--- packages/server/src/repositories/Person.ts
+++ packages/server/src/repositories/Person.ts
@@ -1,25 +1,39 @@
 import type { Person, Prisma } from "@prisma/client";
 import { PrismaClient } from "@prisma/client";
-import type { AuthSource, FilterItem, RoleResource } from "@ukdanceblue/common";
-import { SortDirection } from "@ukdanceblue/common";
+import type { AuthSource } from "@ukdanceblue/common";
+import {
+  AuthIdList,
+  PersonResource,
+  RoleResource,
+  SortDirection,
+} from "@ukdanceblue/common";
 import { Service } from "typedi";
 
 import { findPersonForLogin } from "../lib/auth/findPersonForLogin.js";
+import type { FilterItems } from "../lib/prisma-utils/gqlFilterToPrismaFilter.js";
+import {
+  dateFilterToPrisma,
+  oneOfFilterToPrisma,
+  stringFilterToPrisma,
+} from "../lib/prisma-utils/gqlFilterToPrismaFilter.js";
 
-const personStringKeys = [
-  "name",
-  "email",
-  "linkblue",
-  "dbRole",
-  "committeeRole",
-  "committeeName",
-] as const;
+const personStringKeys = ["name", "email", "linkblue"] as const;
 type PersonStringKey = (typeof personStringKeys)[number];
+
+const personOneOfKeys = ["dbRole", "committeeRole", "committeeName"] as const;
+type PersonOneOfKey = (typeof personOneOfKeys)[number];
 
 const personDateKeys = ["createdAt", "updatedAt"] as const;
 type PersonDateKey = (typeof personDateKeys)[number];
 
-type PersonKey = PersonStringKey | PersonDateKey;
+type PersonFilters = FilterItems<
+  never,
+  PersonDateKey,
+  never,
+  never,
+  PersonOneOfKey,
+  PersonStringKey
+>;
 
 @Service()
 export class PersonRepository {
@@ -54,12 +68,17 @@ export class PersonRepository {
     return this.prisma.person.findUnique({ where: { linkblue } });
   }
 
-  listPeople(
-    filters: readonly FilterItem<PersonKey, unknown>[],
-    order: readonly [key: string, sort: SortDirection][],
-    skip: number,
-    take: number
-  ): Promise<Person[]> {
+  listPeople({
+    filters,
+    order,
+    skip,
+    take,
+  }: {
+    filters: readonly PersonFilters[];
+    order: readonly [key: string, sort: SortDirection][];
+    skip: number;
+    take: number;
+  }): Promise<Person[]> {
     const where: Prisma.PersonWhereInput = {};
     const orderBy: Prisma.PersonOrderByWithRelationInput = {};
 
@@ -67,16 +86,25 @@ export class PersonRepository {
       switch (filter.field) {
         case "name":
         case "email":
-        case "linkblue":
-        case "dbRole":
-        case "committeeRole":
+        case "linkblue": {
+          where[filter.field] = stringFilterToPrisma(filter);
+          break;
+        }
+        case "dbRole": {
+          where[filter.field] = oneOfFilterToPrisma(filter);
+          break;
+        }
+        case "committeeRole": {
+          where[filter.field] = oneOfFilterToPrisma(filter);
+          break;
+        }
         case "committeeName": {
-          where[filter.field] = {};
+          where[filter.field] = oneOfFilterToPrisma(filter);
           break;
         }
         case "createdAt":
         case "updatedAt": {
-          where[filter.field] = filter.value as Date;
+          where[filter.field] = dateFilterToPrisma(filter);
           break;
         }
       }
@@ -89,16 +117,43 @@ export class PersonRepository {
         case "linkblue":
         case "dbRole":
         case "committeeRole":
-        case "committeeName": {
-          orderBy[key] = sort === SortDirection.ASCENDING ? "asc" : "desc";
-          break;
-        }
+        case "committeeName":
         case "createdAt":
         case "updatedAt": {
           orderBy[key] = sort === SortDirection.ASCENDING ? "asc" : "desc";
           break;
         }
+        default: {
+          throw new Error(`Unsupported sort key: ${key}`);
+        }
       }
     }
+
+    return this.prisma.person.findMany({
+      where,
+      orderBy,
+      skip,
+      take,
+    });
+  }
+
+  static personModelToResource(person: Person): PersonResource {
+    return PersonResource.init({
+      uuid: person.uuid,
+      name: person.name,
+      email: person.email,
+      linkblue: person.linkblue,
+      role: RoleResource.init({
+        dbRole: person.dbRole,
+        committeeRole: person.committeeRole,
+        committeeIdentifier: person.committeeName,
+      }),
+      authIds: AuthIdList.isAuthIdListArray(person.authIds)
+        ? person.authIds
+        : [],
+
+      createdAt: person.createdAt,
+      updatedAt: person.updatedAt,
+    });
   }
 }
