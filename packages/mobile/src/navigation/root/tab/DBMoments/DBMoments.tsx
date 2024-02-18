7493608d6cfb28141ee097ad144cb27ae3b523da
Jacksonnnnn <jackson.huse@stu.jefferson.kyschools.us>
2024-02-18 17:58:33 -0500

    Camera fully operational heck yeah tech yeah

--- packages/mobile/src/navigation/root/tab/DBMoments/DBMoments.tsx
+++ packages/mobile/src/navigation/root/tab/DBMoments/DBMoments.tsx
@@ -1,13 +1,31 @@
-import { Box, Button, Fab, Icon, Image, Text, View } from "native-base";
-import { Camera, CameraType } from "expo-camera";
-import { useState } from "react";
 import dbWordLogo from "@assets/logo/big-words.png";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import type { CameraCapturedPicture} from "expo-camera";
+import { Camera, CameraType, FlashMode } from "expo-camera";
+import { Box, Button, Fab, Icon, Image, Text, View } from "native-base";
+import { useRef, useState } from "react";
+import { ImageBackground, TouchableOpacity, useWindowDimensions } from "react-native";
 
 export const DBMomentsScreen = () => {
   const [cameraType, setCameraType] = useState(CameraType.back);
+  const [flash, setFlash] = useState(FlashMode.off);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [cameraReady, setCameraReady] = useState(false);
+  const { height: screenHeight } = useWindowDimensions();
+  const [capturedImage, setCapturedImage] = useState<CameraCapturedPicture>();
+  const [previewVisible, setPreviewVisible] = useState(false);
+
+  const cameraRef = useRef<Camera>(null);
+
+  // relative heights based on jackson's iphone aspect ratio
+  const headerBias = (98 / 932) * screenHeight;
+  const invHeaderBias = screenHeight - headerBias;
+  const navBias = ((932 - 94) / 932) * screenHeight; // will not need once nav is changed
+  const invNavBias = screenHeight - navBias; // will not need once nav is changed
+
+  let flashComponent = (
+    <Fab renderInPortal={false} style={{height: 40, width: 40, backgroundColor: 'rgba(0, 0, 0, 0.2)' }} top={5+65} right={2} icon={<Icon color="white" as={<MaterialIcons name="flash-off"/>} size={7} />} onPress={toggleFlash}/>
+  );
 
   if (!permission) {
     // Camera permissions are still loading
@@ -41,18 +59,115 @@ export const DBMomentsScreen = () => {
   }
 
   function toggleCameraType() {
+    // just to figure out maths for FAB placements
+    console.log(`total height: ${screenHeight}`);
+    console.log(`header bias: ${headerBias}`);
+    console.log(`inv header bias: ${invHeaderBias}`);
+    console.log(`nav bias: ${navBias}`);
+    console.log(`inv nav bias: ${invNavBias}`);
+
     setCameraType(current => (current === CameraType.front ? CameraType.back : CameraType.front));
+  }
+
+  function toggleFlash() {
+    if (flash === FlashMode.on) {
+      flashComponent = (
+        <Fab renderInPortal={false} style={{height: 40, width: 40, backgroundColor: 'rgba(0, 0, 0, 0.2)' }} top={5+65} right={2} icon={<Icon color="white" as={<MaterialIcons name="flash-off"/>} size={7} />} onPress={toggleFlash}/>
+      );
+      console.log("turning flash off");
+      setFlash(FlashMode.off);
+    } else {
+      flashComponent = (
+        <Fab renderInPortal={false} style={{height: 40, width: 40, backgroundColor: 'rgba(0, 0, 0, 0.2)' }} top={5+65} right={2} icon={<Icon color="white" as={<MaterialIcons name="flash-on"/>} size={7} />} onPress={toggleFlash}/>
+      );
+      console.log("turning flash on");
+      setFlash(FlashMode.on);
+    }
+  }
+
+  const onCameraReady = () => {
+    setCameraReady(true);
+  }
+
+  const takePicture = async () => {
+    let photo = null;
+
+    if (cameraRef.current && cameraReady) {
+      const options = { quality: 1, base64: true, skipProcessing: true };
+
+      const captured: Promise<CameraCapturedPicture> = await cameraRef.current.takePictureAsync(options);
+      photo = (await captured);
+      console.log(photo.uri);
+    }
+
+    if (photo) {
+      setPreviewVisible(true);
+      setCapturedImage(photo);
+    } else {
+      console.log("photo is null idk");
+    }
+  }
+
+  const PreviewPicture = () => {
+    console.log('sdsfds', capturedImage?.uri)
+    if (capturedImage) {
+    return (
+      <View style={{backgroundColor: 'transparent', flex: 1, width: '100%', height: '100%'}}>
+        <ImageBackground source={{uri: capturedImage.uri}} style={{flex: 1}}>
+          <View style={{flex: 1, flexDirection: 'column', padding: 15, justifyContent: 'flex-end'}}>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <TouchableOpacity
+                onPress={retakeMoment}
+                style={{
+                  width: 130,
+                  height: 40,
+                  alignItems: 'center',
+                  borderRadius: 4
+                }}>
+                <Text style={{color: '#fff',fontSize: 20}}>Re-take</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={saveMoment}
+                style={{
+                  width: 130,
+                  height: 40,
+                  alignItems: 'center',
+                  borderRadius: 4
+                }}>
+                <Text style={{color: '#fff', fontSize: 20}}>Save Moment</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ImageBackground>
+      </View>
+    )
+  }
+  }
+
+  function saveMoment() {
+
+  }
+
+  const retakeMoment = () => {
+    setCapturedImage(null);
+    setPreviewVisible(false);
   }
 
   return (
     <View style={{flex: 1, justifyContent: "center"}}>
-      <Camera style={{flex: 1}} type={cameraType}>
-        <Box style={{flex: 1, marginTop: 25}}>
-          <Fab style={{padding: 0}} placement={"top-right"} icon={<Icon color="white" as={<MaterialIcons name="cameraswitch"/>} size={5} />} onPress={toggleCameraType}/>
-          <Fab style={{padding: 0}} placement={"bottom-right"} icon={<Icon color="white" as={<MaterialIcons name="cameraswitch"/>} size={5} />} onPress={toggleCameraType}/>
-          <Fab style={{padding: 0}} placement={"bottom-left"} icon={<Icon color="white" as={<MaterialIcons name="cameraswitch"/>} size={5} />} onPress={toggleCameraType}/>
+      {previewVisible && capturedImage ? (
+        <PreviewPicture/>
+      ) : (
+      <Camera style={{flex: 1}} type={cameraType} ref={cameraRef} onCameraReady={onCameraReady} flashMode={flash}>
+        <Box style={{flex: 1}}>
+          <Fab renderInPortal={false} style={{height: 40, width: 40, backgroundColor: 'rgba(0, 0, 0, 0.2)' }} top={5} right={2} icon={<Icon color="white" as={<MaterialIcons name="cameraswitch"/>} size={7} />} onPress={toggleCameraType}/>
+          {flashComponent}
+          <Fab renderInPortal={false} style={{height: 40, width: 40, backgroundColor: 'rgba(0, 0, 0, 0.2)' }} top={65+50+5} right={2} icon={<Icon color="white" as={<MaterialIcons name="info"/>} size={7} />} onPress={toggleCameraType}/>
+          <Fab renderInPortal={false} style={{height: 50, width: 50, backgroundColor: 'rgba(0, 0, 0, 0.2)'  }} bottom={5} right={2} icon={<Icon color="white" as={<MaterialCommunityIcons name="circle-slice-8"/>} size={10} />} onPress={takePicture}/>
+          <Fab renderInPortal={false} style={{height: 50, width: 50, backgroundColor: 'rgba(0, 0, 0, 0.2)' }} bottom={5} left={2} icon={<Icon color="white" as={<MaterialCommunityIcons name="dots-circle"/>} size={10} />} onPress={toggleCameraType}/>
         </Box>
       </Camera>
+    )}
     </View>
   );
 };
