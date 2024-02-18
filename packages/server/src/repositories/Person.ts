512c31e7cbc4f9ac3c13260ab113259950c24972
Joshua Tag Howard <tag@jthoward.dev>
2024-02-17 23:20:24 -0500

    Refactor PersonRepository query methods and add countPeople method

--- packages/server/src/repositories/Person.ts
+++ packages/server/src/repositories/Person.ts
@@ -74,67 +74,31 @@ export class PersonRepository {
     skip,
     take,
   }: {
-    filters: readonly PersonFilters[];
-    order: readonly [key: string, sort: SortDirection][];
-    skip: number;
-    take: number;
+    filters?: readonly PersonFilters[] | undefined | null;
+    order?: readonly [key: string, sort: SortDirection][] | undefined | null;
+    skip?: number | undefined | null;
+    take?: number | undefined | null;
   }): Promise<Person[]> {
-    const where: Prisma.PersonWhereInput = {};
-    const orderBy: Prisma.PersonOrderByWithRelationInput = {};
-
-    for (const filter of filters) {
-      switch (filter.field) {
-        case "name":
-        case "email":
-        case "linkblue": {
-          where[filter.field] = stringFilterToPrisma(filter);
-          break;
-        }
-        case "dbRole": {
-          where[filter.field] = oneOfFilterToPrisma(filter);
-          break;
-        }
-        case "committeeRole": {
-          where[filter.field] = oneOfFilterToPrisma(filter);
-          break;
-        }
-        case "committeeName": {
-          where[filter.field] = oneOfFilterToPrisma(filter);
-          break;
-        }
-        case "createdAt":
-        case "updatedAt": {
-          where[filter.field] = dateFilterToPrisma(filter);
-          break;
-        }
-      }
-    }
-
-    for (const [key, sort] of order) {
-      switch (key) {
-        case "name":
-        case "email":
-        case "linkblue":
-        case "dbRole":
-        case "committeeRole":
-        case "committeeName":
-        case "createdAt":
-        case "updatedAt": {
-          orderBy[key] = sort === SortDirection.ASCENDING ? "asc" : "desc";
-          break;
-        }
-        default: {
-          throw new Error(`Unsupported sort key: ${key}`);
-        }
-      }
-    }
+    const where: Prisma.PersonWhereInput = buildPersonWhere(filters);
+    const orderBy: Prisma.PersonOrderByWithRelationInput =
+      buildPersonOrder(order);
 
     return this.prisma.person.findMany({
       where,
       orderBy,
-      skip,
-      take,
+      skip: skip ?? undefined,
+      take: take ?? undefined,
     });
+  }
+
+  countPeople({
+    filters,
+  }: {
+    filters?: readonly PersonFilters[] | undefined | null;
+  }): Promise<number> {
+    const where: Prisma.PersonWhereInput = buildPersonWhere(filters);
+
+    return this.prisma.person.count({ where });
   }
 
   static personModelToResource(person: Person): PersonResource {
@@ -156,4 +120,87 @@ export class PersonRepository {
       updatedAt: person.updatedAt,
     });
   }
+
+  searchByName(name: string): Promise<Person[]> {
+    return this.prisma.person.findMany({
+      where: {
+        name: {
+          contains: name,
+          mode: "insensitive",
+        },
+      },
+    });
+  }
+
+  searchByLinkblue(linkblue: string): Promise<Person[]> {
+    return this.prisma.person.findMany({
+      where: {
+        linkblue: {
+          contains: linkblue,
+          mode: "insensitive",
+        },
+      },
+    });
+  }
+}
+
+function buildPersonOrder(
+  order: readonly [key: string, sort: SortDirection][] | null | undefined
+) {
+  const orderBy: Prisma.PersonOrderByWithRelationInput = {};
+
+  for (const [key, sort] of order ?? []) {
+    switch (key) {
+      case "name":
+      case "email":
+      case "linkblue":
+      case "dbRole":
+      case "committeeRole":
+      case "committeeName":
+      case "createdAt":
+      case "updatedAt": {
+        orderBy[key] = sort === SortDirection.ASCENDING ? "asc" : "desc";
+        break;
+      }
+      default: {
+        throw new Error(`Unsupported sort key: ${key}`);
+      }
+    }
+  }
+  return orderBy;
+}
+
+function buildPersonWhere(
+  filters: readonly PersonFilters[] | null | undefined
+) {
+  const where: Prisma.PersonWhereInput = {};
+
+  for (const filter of filters ?? []) {
+    switch (filter.field) {
+      case "name":
+      case "email":
+      case "linkblue": {
+        where[filter.field] = stringFilterToPrisma(filter);
+        break;
+      }
+      case "dbRole": {
+        where[filter.field] = oneOfFilterToPrisma(filter);
+        break;
+      }
+      case "committeeRole": {
+        where[filter.field] = oneOfFilterToPrisma(filter);
+        break;
+      }
+      case "committeeName": {
+        where[filter.field] = oneOfFilterToPrisma(filter);
+        break;
+      }
+      case "createdAt":
+      case "updatedAt": {
+        where[filter.field] = dateFilterToPrisma(filter);
+        break;
+      }
+    }
+  }
+  return where;
 }
