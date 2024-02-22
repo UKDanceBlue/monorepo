f66f94e0068a2409974f69311825f4647bf05591
Joshua Tag Howard <tag@jthoward.dev>
2024-02-22 17:58:39 -0500

    Implement *ModelToResource for a bunch of models

--- packages/server/src/repositories/image/imageModelToResource.ts
+++ packages/server/src/repositories/image/imageModelToResource.ts
@@ -4,5 +4,14 @@ import { ImageResource } from "@ukdanceblue/common";
 export function imageModelToResource(imageModel: Image): ImageResource {
   return ImageResource.init({
     uuid: imageModel.uuid,
+    url: imageModel.url ? new URL(imageModel.url) : null,
+    imageData: imageModel.imageData?.toString("base64"),
+    mimeType: imageModel.mimeType ?? undefined,
+    thumbHash: imageModel.thumbHash?.toString("base64"),
+    alt: imageModel.alt ?? undefined,
+    width: imageModel.width,
+    height: imageModel.height,
+    createdAt: imageModel.createdAt,
+    updatedAt: imageModel.updatedAt,
   });
 }
--- packages/server/src/repositories/pointOpportunity/pointOpportunityModelToResource.ts
+++ packages/server/src/repositories/pointOpportunity/pointOpportunityModelToResource.ts
@@ -1,8 +1,18 @@
 import type { PointOpportunity } from "@prisma/client";
 import { PointOpportunityResource } from "@ukdanceblue/common";
+import { DateTime } from "luxon";
 
-export function pointOpportunityModelToResource(pointOpportunityModel: PointOpportunity): PointOpportunityResource {
+export function pointOpportunityModelToResource(
+  pointOpportunityModel: PointOpportunity
+): PointOpportunityResource {
   return PointOpportunityResource.init({
     uuid: pointOpportunityModel.uuid,
+    name: pointOpportunityModel.name,
+    type: pointOpportunityModel.type,
+    opportunityDate: pointOpportunityModel.opportunityDate
+      ? DateTime.fromJSDate(pointOpportunityModel.opportunityDate)
+      : undefined,
+    createdAt: pointOpportunityModel.createdAt,
+    updatedAt: pointOpportunityModel.updatedAt,
   });
 }
