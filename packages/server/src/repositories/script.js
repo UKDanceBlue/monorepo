9adfa00f153626949cf10851cf69b8e2aa9dbfcb
Joshua Tag Howard <tag@jthoward.dev>
2024-02-23 07:59:54 +0000

    Change around the repository script

--- packages/server/src/repositories/script.js
+++ packages/server/src/repositories/script.js
@@ -1,37 +1,26 @@
 import fs from "fs";
 import path from "path";
 
-const dir = fs.readdirSync(".");
-
-for (const folder of dir.filter((f) => fs.lstatSync(f).isDirectory())) {
+for (const folder of [
+  "event",
+  "image",
+  "membership",
+  "notification",
+  "pointEntry",
+  "pointOpportunity",
+  "team",
+]) {
   const upperFolder = folder.charAt(0).toUpperCase() + folder.slice(1);
-  // skip if non-empty
-  if (fs.readdirSync(folder).length > 0) {
-    continue;
-  }
 
-  console.log(`Creating files for ${folder}`);
+  console.log(`Replacing files for ${folder}`);
 
   // Folder name
   const folderPath = path.resolve(folder);
 
   fs.writeFileSync(
-    path.join(folderPath, `${folder}ModelToResource.ts`),
-    `import type { ${upperFolder} } from "@prisma/client";
-import { ${upperFolder}Resource } from "@ukdanceblue/common";
-
-export function ${folder}ModelToResource(${folder}Model: ${upperFolder}): ${upperFolder}Resource {
-  return ${upperFolder}Resource.init({
-    uuid: ${folder}Model.uuid,
-  });
-}
-`
-  );
-
-  fs.writeFileSync(
     path.join(folderPath, `${upperFolder}Repository.ts`),
     `
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import type { SortDirection } from "@ukdanceblue/common";
 import { Service } from "typedi";
 
@@ -66,24 +55,97 @@ export type ${upperFolder}Filters = FilterItems<
   ${upperFolder}StringKey
 >;
 
+type Unique${upperFolder}Param = { id: number } | { uuid: string };
+
 @Service()
 export class ${upperFolder}Repository {
   constructor(
     private prisma: PrismaClient,
     ) {}
+
+    find${upperFolder}ByUnique(param: Unique${upperFolder}Param) {
+      return this.prisma.${folder}.findUnique({where: param});
+    }
+
+    list${upperFolder}({
+      filters,
+      order,
+      skip,
+      take,
+    }: {
+      filters?: readonly ${upperFolder}Filters[] | undefined | null;
+      order?: readonly [key: string, sort: SortDirection][] | undefined | null;
+      skip?: number | undefined | null;
+      take?: number | undefined | null;
+    }) {
+      const where = build${upperFolder}Where(filters);
+      const orderBy = build${upperFolder}Order(order);
+
+      return this.prisma.${folder}.findMany({
+        where,
+        orderBy,
+        skip: skip ?? undefined,
+        take: take ?? undefined,
+      });
+    }
+
+    count${upperFolder}({
+      filters,
+    }: {
+      filters?: readonly ${upperFolder}Filters[] | undefined | null;
+    }) {
+      const where = build${upperFolder}Where(filters);
+
+      return this.prisma.${folder}.count({
+        where,
+      });
+    }
+
+    create${upperFolder}(data: Prisma.${upperFolder}CreateInput) {
+      return this.prisma.${folder}.create({ data });
+    }
+
+    update${upperFolder}(param: Unique${upperFolder}Param, data: Prisma.${upperFolder}UpdateInput) {
+      try {
+        return this.prisma.${folder}.update({ where: param, data });
+      } catch (error) {
+        if (
+          error instanceof Prisma.PrismaClientKnownRequestError &&
+          error.code === "P2025"
+        ) {
+          return null;
+        } else {
+          throw error;
+        }
+      }
+    }
+
+    delete${upperFolder}(param: Unique${upperFolder}Param) {
+      try {
+        return this.prisma.${folder}.delete({ where: param });
+      } catch (error) {
+        if (
+          error instanceof Prisma.PrismaClientKnownRequestError &&
+          error.code === "P2025"
+        ) {
+          return null;
+        } else {
+          throw error;
+        }
+      }
+    }
 }`
   );
 
   fs.writeFileSync(
     path.join(folderPath, `${folder}RepositoryUtils.ts`),
-    `import type { ${upperFolder}Filters } from "./${upperFolder}Repository.ts";
-import type { ${upperFolder}, Prisma } from "@prisma/client";
+    `import type { Prisma } from "@prisma/client";
 import { SortDirection } from "@ukdanceblue/common";
-import { buildFilter } from "../../lib/prisma-utils/gqlFilterToPrismaFilter.js";
+
+import type { ${upperFolder}Filters } from "./${upperFolder}Repository.ts";
 
 export function build${upperFolder}Order(
-  order: string,
-  direction: SortDirection
+  order: readonly [key: string, sort: SortDirection][] | null | undefined
 ) {
   const orderBy: Prisma.${upperFolder}OrderByWithRelationInput = {};
 
@@ -103,7 +165,7 @@ export function build${upperFolder}Order(
 }
 
 export function build${upperFolder}Where(
-  filters: ${upperFolder}Filters[]
+  filters: readonly ${upperFolder}Filters[] | null | undefined
 ) {
   const where: Prisma.${upperFolder}WhereInput = {};
 
