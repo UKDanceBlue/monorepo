b25548367daf43311e8d2dbac2c3ceac59dff05c
Joshua Tag Howard <tag@jthoward.dev>
2024-02-25 10:46:33 +0000

    Remove console.log statement in prisma.ts

--- packages/server/src/prisma.ts
+++ packages/server/src/prisma.ts
@@ -7,6 +7,4 @@ Container.set<typeof prisma>(PrismaClient, prisma);
 
 if (!Container.has(PrismaClient)) {
   throw new Error("PrismaClient not registered");
-} else {
-  console.log("PrismaClient registered");
 }
