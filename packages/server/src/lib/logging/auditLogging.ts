c7c2ec44ea258cbd26b5223a794f039a77f65fdd
Joshua Tag Howard <tag@jthoward.dev>
2024-02-25 01:01:40 +0000

    Update auditLogger in auditLogging.ts

--- packages/server/src/lib/logging/auditLogging.ts
+++ packages/server/src/lib/logging/auditLogging.ts
@@ -1,13 +1,39 @@
 import type { LeveledLogMethod, Logger } from "winston";
-import { createLogger, format } from "winston";
+import { createLogger, format, transports } from "winston";
 
 import { isDevelopment } from "../../environment.js";
 
-import { auditLogTransport } from "./transports/fileLogTransports.js";
-
 export interface AuditLogger extends Logger {
+  /**
+   * Log a message with the level `insecure`
+   *
+   * Use this level for an action that can be taken
+   * by an arbitrary user, but is notable
+   */
   insecure: LeveledLogMethod;
+  /**
+   * Log a message with the level `secure`
+   *
+   * Use this level for an action that can only be
+   * taken by an authorized user (i.e. a committee
+   * member or an admin)
+   */
   secure: LeveledLogMethod;
+  /**
+   * Log a message with the level `secure`
+   *
+   * Use this level for an action that can only be
+   * taken by an authorized user (i.e. a committee
+   * member or an admin) and is a destructive action
+   */
+  dangerous: LeveledLogMethod;
+  /**
+   * Log a message with the level `info`
+   *
+   * Use this level for general information
+   * about the operation of the server or
+   * the logger itself
+   */
   info: LeveledLogMethod;
 
   warn: never;
@@ -26,15 +52,22 @@ export interface AuditLogger extends Logger {
   notice: never;
 }
 
+const auditLogTransport = new transports.File({
+  filename: "audit.log.json",
+  maxsize: 1_000_000,
+  maxFiles: 3,
+});
+
 export const auditLogger = createLogger({
   level: "secure",
   silent: isDevelopment,
   transports: auditLogTransport,
   format: format.combine(format.timestamp(), format.json()),
   levels: {
-    insecure: 0,
-    secure: 1,
-    info: 2,
+    info: 0,
+    dangerous: 2,
+    secure: 4,
+    insecure: 6,
   },
 }) as AuditLogger;
 
