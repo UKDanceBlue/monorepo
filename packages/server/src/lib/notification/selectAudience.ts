93136753c29716aa2ffd4da2f11ca2e17e63b9d1
Joshua Tag Howard <tag@jthoward.dev>
2024-02-25 02:48:39 +0000

    Refactor selectAudience function signature

--- packages/server/src/lib/notification/selectAudience.ts
+++ packages/server/src/lib/notification/selectAudience.ts
@@ -1,6 +1,8 @@
+import type { Notification } from "@prisma/client";
+
 // Stub for now
 export async function selectAudience(
-  _notification: NotificationModel,
+  _notification: Notification,
   _audience: unknown
 ): Promise<string[]> {
   // eslint-disable-next-line unicorn/no-useless-promise-resolve-reject
