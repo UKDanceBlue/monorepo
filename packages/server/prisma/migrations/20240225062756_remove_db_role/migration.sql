bcc797d1b19d475d502b42ec393d919604d75d75
Joshua Tag Howard <tag@jthoward.dev>
2024-02-25 07:12:29 +0000

    Remove DB Role from database

--- packages/server/prisma/migrations/20240225062756_remove_db_role/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `db_role` on the `people` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "people" DROP COLUMN "db_role";
