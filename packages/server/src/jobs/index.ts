4f4cb2a4297c5adaa6d997fc14edb1eb9d38f538
Joshua Tag Howard <tag@jthoward.dev>
2024-03-05 09:46:18 +0000

    Add NotificationScheduler to job index

--- packages/server/src/jobs/index.ts
+++ packages/server/src/jobs/index.ts
@@ -1,5 +1,10 @@
+import { Container } from "typedi";
+
+import { NotificationScheduler } from "./NotificationScheduler.js";
 import { fetchPushReceipts } from "./fetchPushReceipts.js";
 import { garbageCollectLoginFlowSessions } from "./garbageCollectLogins.js";
 
 await fetchPushReceipts.trigger();
 await garbageCollectLoginFlowSessions.trigger();
+const scheduler = Container.get(NotificationScheduler);
+scheduler.ensureNotificationScheduler();
