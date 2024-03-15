6118d6008fe7c1f02ba9337f1ab47148c53c085b
Joshua Tag Howard <tag@jthoward.dev>
2024-03-15 17:42:37 +0000

    Add MarathonHourRepository and update docker-compose.yml

---
+++ packages/server/src/repositories/marathonHour/MarathonHourRepository.ts
@@ -0,0 +1,164 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import type { SortDirection } from "@ukdanceblue/common";
+import { Service } from "typedi";
+
+import type { FilterItems } from "../../lib/prisma-utils/gqlFilterToPrismaFilter.js";
+
+import {
+  buildMarathonHourOrder,
+  buildMarathonHourWhere,
+} from "./marathonHourRepositoryUtils.js";
+
+const marathonHourBooleanKeys = [] as const;
+type MarathonHourBooleanKey = (typeof marathonHourBooleanKeys)[number];
+
+const marathonHourDateKeys = [
+  "shownStartingAt",
+  "createdAt",
+  "updatedAt",
+] as const;
+type MarathonHourDateKey = (typeof marathonHourDateKeys)[number];
+
+const marathonHourIsNullKeys = [] as const;
+type MarathonHourIsNullKey = (typeof marathonHourIsNullKeys)[number];
+
+const marathonHourNumericKeys = [] as const;
+type MarathonHourNumericKey = (typeof marathonHourNumericKeys)[number];
+
+const marathonHourOneOfKeys = ["marathonYear"] as const;
+type MarathonHourOneOfKey = (typeof marathonHourOneOfKeys)[number];
+
+const marathonHourStringKeys = ["title", "details", "durationInfo"] as const;
+type MarathonHourStringKey = (typeof marathonHourStringKeys)[number];
+
+export type MarathonHourOrderKeys =
+  | "title"
+  | "details"
+  | "durationInfo"
+  | "marathonYear"
+  | "shownStartingAt"
+  | "createdAt"
+  | "updatedAt";
+
+export type MarathonHourFilters = FilterItems<
+  MarathonHourBooleanKey,
+  MarathonHourDateKey,
+  MarathonHourIsNullKey,
+  MarathonHourNumericKey,
+  MarathonHourOneOfKey,
+  MarathonHourStringKey
+>;
+
+type UniqueParam = { id: number } | { uuid: string };
+
+@Service()
+export class MarathonHourRepository {
+  constructor(private prisma: PrismaClient) {}
+
+  findMarathonHourByUnique(param: UniqueParam) {
+    return this.prisma.marathonHour.findUnique({ where: param });
+  }
+
+  listMarathonHours({
+    filters,
+    order,
+    skip,
+    take,
+  }: {
+    filters?: readonly MarathonHourFilters[] | undefined | null;
+    order?:
+      | readonly [key: MarathonHourOrderKeys, sort: SortDirection][]
+      | undefined
+      | null;
+    skip?: number | undefined | null;
+    take?: number | undefined | null;
+  }) {
+    const where = buildMarathonHourWhere(filters);
+    const orderBy = buildMarathonHourOrder(order);
+
+    return this.prisma.marathonHour.findMany({
+      where,
+      orderBy,
+      skip: skip ?? undefined,
+      take: take ?? undefined,
+    });
+  }
+
+  createMarathonHour({
+    title,
+    details,
+    marathon,
+    shownStartingAt,
+    durationInfo,
+  }: {
+    title: string;
+    details?: string | undefined;
+    marathon: UniqueParam;
+    shownStartingAt: Date;
+    durationInfo: string;
+  }) {
+    return this.prisma.marathonHour.create({
+      data: {
+        title,
+        details,
+        marathon: { connect: marathon },
+        shownStartingAt,
+        durationInfo,
+      },
+    });
+  }
+
+  updateMarathonHour(
+    param: UniqueParam,
+    {
+      title,
+      details,
+      marathon,
+      shownStartingAt,
+      durationInfo,
+    }: {
+      title?: string | undefined;
+      details?: string | undefined | null;
+      marathon?: UniqueParam | undefined;
+      shownStartingAt?: Date | undefined;
+      durationInfo?: string | undefined;
+    }
+  ) {
+    try {
+      return this.prisma.marathonHour.update({
+        where: param,
+        data: {
+          title,
+          details,
+          marathon: marathon ? { connect: marathon } : undefined,
+          shownStartingAt,
+          durationInfo,
+        },
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === "P2025"
+      ) {
+        return null;
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  deleteMarathonHour(param: UniqueParam) {
+    try {
+      return this.prisma.marathonHour.delete({ where: param });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === "P2025"
+      ) {
+        return null;
+      } else {
+        throw error;
+      }
+    }
+  }
+}
