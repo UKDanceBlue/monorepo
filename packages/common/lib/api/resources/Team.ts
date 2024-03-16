52c36f1c842f6baa64b2c302e480f6cd870eebf2
Joshua Tag Howard <tag@jthoward.dev>
2024-03-16 19:54:18 +0000

    Refactor file layout in common

---
+++ packages/common/lib/api/resources/Team.ts
@@ -0,0 +1,60 @@
+import { Field, ID, ObjectType, registerEnumType } from "type-graphql";
+
+import { AccessLevel } from "../../authorization/structures.js";
+import * as SimpleTypes from "../../utility/primitive/SimpleTypes.js";
+
+import { AccessControl } from "../../authorization/accessControl.js";
+import { TimestampedResource } from "./Resource.js";
+
+export const TeamType = {
+  Spirit: "Spirit",
+  Morale: "Morale",
+  Committee: "Committee",
+} as const;
+export type TeamType = (typeof TeamType)[keyof typeof TeamType];
+
+// Registering the TeamType enum with TypeGraphQL
+registerEnumType(TeamType, {
+  name: "TeamType",
+  description: "Types of teams",
+});
+
+export const TeamLegacyStatus = {
+  NewTeam: "NewTeam",
+  ReturningTeam: "ReturningTeam",
+  DemoTeam: "DemoTeam",
+} as const;
+export type TeamLegacyStatus =
+  (typeof TeamLegacyStatus)[keyof typeof TeamLegacyStatus];
+
+// Registering the TeamLegacyStatus enum with TypeGraphQL
+registerEnumType(TeamLegacyStatus, {
+  name: "TeamLegacyStatus",
+  description: "New Team vs Returning Team",
+});
+
+@ObjectType()
+export class TeamResource extends TimestampedResource {
+  @Field(() => ID)
+  uuid!: string;
+  @Field(() => String)
+  name!: string;
+  @Field(() => TeamType)
+  type!: TeamType;
+  @Field(() => TeamLegacyStatus)
+  legacyStatus!: TeamLegacyStatus;
+  @Field(() => String)
+  marathonYear!: SimpleTypes.MarathonYearString;
+
+  @AccessControl({ accessLevel: AccessLevel.Committee })
+  @Field(() => String, { nullable: true })
+  persistentIdentifier!: string | null;
+
+  public getUniqueId(): string {
+    return this.uuid;
+  }
+
+  public static init(init: Partial<TeamResource>) {
+    return TeamResource.doInit(init);
+  }
+}
