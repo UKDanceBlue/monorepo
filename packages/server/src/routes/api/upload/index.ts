9d1e1117f10ac219b8a351f21f2e0f44e465e009
Joshua Tag Howard <tag@jthoward.dev>
2024-03-27 23:25:17 +0000

    Refactor seed script and remove unnecessary code

--- packages/server/src/routes/api/upload/index.ts
+++ packages/server/src/routes/api/upload/index.ts
@@ -1,28 +1,35 @@
 import Router from "@koa/router";
+import { koaBody } from "koa-body";
 import { Container } from "typedi";
 
-import { koaBody } from "koa-body";
 import { ImageRepository } from "../../../repositories/image/ImageRepository.js";
 
 const uploadRouter = new Router({ prefix: "/upload" });
 
 // Multipart image upload
-uploadRouter.post("/image/:uuid",koaBody({multipart: true,formidable: {
-  allowEmptyFiles: false,
-}}), async (ctx) => {
-  const imageRepository = Container.get(ImageRepository);
+uploadRouter.post(
+  "/image/:uuid",
+  koaBody({
+    multipart: true,
+    formidable: {
+      allowEmptyFiles: false,
+    },
+  }),
+  async (ctx) => {
+    const imageRepository = Container.get(ImageRepository);
 
-  const { uuid } = ctx.params;
+    const { uuid } = ctx.params;
 
-  const image = ctx.request.files?.image;
+    const image = ctx.request.files?.image;
 
-  if (!image) {
-    ctx.throw(400, "No image uploaded");
-  } else if (Array.isArray(image)) {
-    ctx.throw(400, "Only one file can be attached to an image");
-  } else {
-    image.
+    if (!image) {
+      ctx.throw(400, "No image uploaded");
+    } else if (Array.isArray(image)) {
+      ctx.throw(400, "Only one file can be attached to an image");
+    } else {
+      // image.
+    }
   }
-});
+);
 
 export default uploadRouter;
--- packages/server/src/seed.ts
+++ packages/server/src/seed.ts
@@ -1,92 +1,101 @@
-import {
-  CommitteeIdentifier,
-  CommitteeRole,
-  TeamLegacyStatus,
-  TeamType,
-} from "@ukdanceblue/common";
+import { CommitteeIdentifier, CommitteeRole } from "@ukdanceblue/common";
 import { Container } from "typedi";
 
 import { isDevelopment } from "./environment.js";
 import { ConfigurationRepository } from "./repositories/configuration/ConfigurationRepository.js";
 import { PersonRepository } from "./repositories/person/PersonRepository.js";
-import { TeamRepository } from "./repositories/team/TeamRepository.js";
 
 if (!isDevelopment) {
   throw new Error("Seeding is only allowed in development mode");
 }
 
-await import("./prisma.js");
+const { prisma } = await import("./prisma.js");
 
-const personRepository = Container.get(PersonRepository);
+try {
+  const personRepository = Container.get(PersonRepository);
 
-const techCommittee: [string, string][] = [
-  ["jtho264@uky.edu", "jtho264"],
-  ["jp.huse@uky.edu", "jphu235"],
-  ["bartholomai@uky.edu", "mdba238"],
-  ["Camille.Dyer@uky.edu", "chdy223"],
-  ["str249@uky.edu", "str249"],
-];
+  const techCommittee: [string, string][] = [
+    ["jtho264@uky.edu", "jtho264"],
+    ["jp.huse@uky.edu", "jphu235"],
+    ["bartholomai@uky.edu", "mdba238"],
+    ["Camille.Dyer@uky.edu", "chdy223"],
+    ["str249@uky.edu", "str249"],
+  ];
 
-const randos: [string, string, string][] = [
-  ["abcd123@uky.edu", "abcd123", "Johnny Appleseed"],
-  ["dcba321@uky.edu", "dcba321", "Jane Doe"],
-  ["mike@uky.edu", "mike123", "Mike"],
-];
+  const randoms: [string, string, string][] = [
+    ["abcd123@uky.edu", "abcd123", "Johnny Appleseed"],
+    ["dcba321@uky.edu", "dcba321", "Jane Doe"],
+    ["mike@uky.edu", "mike123", "Mike"],
+  ];
 
-const techPeople = await Promise.all(
-  techCommittee.map(async ([email, linkblue]) =>
-    personRepository.createPerson({
-      email,
-      linkblue,
-      committeeRole: CommitteeRole.Coordinator,
-      committeeName: CommitteeIdentifier.techCommittee,
-    })
-  )
-);
+  const techPeople = await Promise.all(
+    techCommittee.map(([email, linkblue]) =>
+      personRepository.createPerson({
+        email,
+        linkblue,
+        committeeRole: CommitteeRole.Coordinator,
+        committeeName: CommitteeIdentifier.techCommittee,
+      })
+    )
+  );
 
-const randoPeople = await Promise.all(
-  randos.map(async ([email, linkblue, name]) =>
-    personRepository.createPerson({
-      email,
-      linkblue,
-      name,
-    })
-  )
-);
+  const randomPeople = await Promise.all(
+    randoms.map(([email, linkblue, name]) =>
+      personRepository.createPerson({
+        email,
+        linkblue,
+        name,
+      })
+    )
+  );
 
-const teamRepository = Container.get(TeamRepository);
+  if (randomPeople.length !== randoms.length) {
+    throw new Error("Failed to create all random people");
+  }
+  if (techPeople.length !== techCommittee.length) {
+    throw new Error("Failed to create all tech committee people");
+  }
 
-await teamRepository.createTeam({
-  name: "Tech Committee",
-  legacyStatus: TeamLegacyStatus.ReturningTeam,
-  marathonYear: "DB24",
-  type: TeamType.Committee,
-  memberships: {
-    connect: techPeople,
-  },
-});
+  // const teamRepository = Container.get(TeamRepository);
 
-await teamRepository.createTeam({
-  name: "Random People",
-  legacyStatus: TeamLegacyStatus.NewTeam,
-  marathonYear: "DB24",
-  type: TeamType.Spirit,
-  memberships: {
-    connect: randoPeople,
-  },
-});
+  // await teamRepository.createTeam({
+  //   name: "Tech Committee",
+  //   legacyStatus: TeamLegacyStatus.ReturningTeam,
+  //   marathonYear: "DB24",
+  //   type: TeamType.Committee,
+  //   memberships: {
+  //     connect: techPeople,
+  //   },
+  //   persistentIdentifier: CommitteeIdentifier.techCommittee,
+  // });
 
-const configurationRepository = Container.get(ConfigurationRepository);
+  // await teamRepository.createTeam({
+  //   name: "Random People",
+  //   legacyStatus: TeamLegacyStatus.NewTeam,
+  //   marathonYear: "DB24",
+  //   type: TeamType.Spirit,
+  //   memberships: {
+  //     connect: randomPeople,
+  //   },
+  // });
 
-await configurationRepository.createConfiguration({
-  key: "TAB_BAR_CONFIG",
-  validAfter: null,
-  validUntil: null,
-  value: JSON.stringify({ shownTabs: ["Teams", "Events"], fancyTab: "Teams" }),
-});
-await configurationRepository.createConfiguration({
-  key: "ALLOWED_LOGIN_TYPES",
-  validAfter: null,
-  validUntil: null,
-  value: JSON.stringify(["anonymous", "ms-oath-linkblue"]),
-});
+  const configurationRepository = Container.get(ConfigurationRepository);
+
+  await configurationRepository.createConfiguration({
+    key: "TAB_BAR_CONFIG",
+    validAfter: null,
+    validUntil: null,
+    value: JSON.stringify({
+      shownTabs: ["Teams", "Events"],
+      fancyTab: "Teams",
+    }),
+  });
+  await configurationRepository.createConfiguration({
+    key: "ALLOWED_LOGIN_TYPES",
+    validAfter: null,
+    validUntil: null,
+    value: JSON.stringify(["anonymous", "ms-oath-linkblue"]),
+  });
+} finally {
+  await prisma.$disconnect();
+}
