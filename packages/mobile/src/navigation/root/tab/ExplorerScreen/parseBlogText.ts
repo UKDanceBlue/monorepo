746c26d21fe17b5d892c19ccb1fb704fa16d0611
Tag Howard <tag@jthoward.dev>
2024-03-28 10:49:04 -0400

    Explorer page (#99)
    * Created blank page
    
    * Forced Explorer Page to be enabled
    
    * Created ExplorerItem
    
    * Podcast and blog stylings are done
    
    * Added blog and podcast feeds, planned out todos
    
    * Fixed the blog read more button
    
    * Run ESLint auto-fix
    
    * Refactor ExplorerScreen component and add useExplorerFeed hook
    
    * Update useExplorerFeed in ExplorerScreen.tsx
    
    * Add useMemo hook to optimize rendering in ExplorerItem component
    
    * implemented all rss feeds except for instagram and tiktok
    
    * Fixed date issues
    
    * small changes
    
    * Run ESLint
    
    * Simplify ExplorerItem
    
    * Refactor ExplorerScreen and ExplorerItem components
    
    * Add htmlparser2 dependency and implement parseBlogText function
    
    * Update dependencies in package.json
    
    * Update lockfile
    
    ---------
    
    Co-authored-by: Jacksonnnnn <jackson.huse@stu.jefferson.kyschools.us>

---
+++ packages/mobile/src/navigation/root/tab/ExplorerScreen/parseBlogText.ts
@@ -0,0 +1,28 @@
+import { Parser } from "htmlparser2";
+
+export function parseBlogText(
+  text: string,
+  maxLength: number
+): Promise<string> {
+  return new Promise((resolve, reject) => {
+    let parsedText = "";
+    const parser = new Parser({
+      ontext: (text) => {
+        if (parsedText.length < maxLength) {
+          parsedText += text;
+        } else {
+          parser.end();
+          const lastSpace = parsedText.lastIndexOf(" ", maxLength);
+          resolve(`${parsedText.substring(0, lastSpace)} [...]`);
+        }
+      },
+    });
+    try {
+      parser.write(text);
+    } catch (error) {
+      reject(error);
+    } finally {
+      parser.end();
+    }
+  });
+}
