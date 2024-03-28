89596e072b153df7b13e036bf35ddf44eaacd059
Joshua Tag Howard <tag@jthoward.dev>
2024-03-28 06:32:35 +0000

    Add useSaveMoment hook and refactor captureMoment function

--- packages/mobile/src/navigation/root/tab/DBMoments/PreviewMoment.tsx
+++ packages/mobile/src/navigation/root/tab/DBMoments/PreviewMoment.tsx
@@ -1,3 +1,4 @@
+import { Logger } from "@common/logger/Logger";
 import { MaterialIcons } from "@expo/vector-icons";
 import type { CameraCapturedPicture } from "expo-camera";
 import type { View } from "native-base";
@@ -6,6 +7,7 @@ import React, { useRef, useState } from "react";
 import { captureRef } from "react-native-view-shot";
 
 import { HiddenComponent } from "./HiddenComponent";
+import { useSaveMoment } from "./useSaveMoment";
 
 export const PreviewMoment = ({
   frontImg,
@@ -18,23 +20,33 @@ export const PreviewMoment = ({
 }) => {
   const viewRef = useRef<typeof View>(null);
   const [viewSize, setViewSize] = useState({ width: 0, height: 0 });
-  const saveMoment = async () => {
-    console.log("Saving moment...");
-    try {
-      console.log("Capturing image...", {
-        width: viewSize.width,
-        height: viewSize.height,
-      });
-      const uri = await captureRef(viewRef, {
-        format: "jpg", // Specify the format of the captured image
-        quality: 0.8, // Specify the image quality (0.0 to 1.0)
-        width: viewSize.width, // Specify the width of the captured image
-        height: viewSize.height, // Specify the height of the captured image
-      });
-      console.log("Image captured:", uri);
-      // Implement logic to save or share the captured image URI
-    } catch (error) {
-      console.error("Capture failed:", error);
+
+  const { saveMoment, ask } = useSaveMoment();
+
+  const captureMoment = async () => {
+    if (ask) {
+      return ask();
+    }
+    if (saveMoment) {
+      console.log("Saving moment...");
+      try {
+        console.log("Capturing image...", {
+          width: viewSize.width,
+          height: viewSize.height,
+        });
+        const uri = await captureRef(viewRef, {
+          format: "jpg", // Specify the format of the captured image
+          quality: 0.8, // Specify the image quality (0.0 to 1.0)
+          width: viewSize.width, // Specify the width of the captured image
+          height: viewSize.height, // Specify the height of the captured image
+        });
+        console.log("Image captured:", uri);
+
+        // Implement logic to save or share the captured image URI
+        await saveMoment(uri);
+      } catch (error) {
+        console.error("Capture failed:", error);
+      }
     }
   };
 
@@ -58,7 +70,11 @@ export const PreviewMoment = ({
         icon={
           <Icon color="white" as={<MaterialIcons name="save-alt" />} size={7} />
         }
-        onPress={() => saveMoment()}
+        onPress={() =>
+          captureMoment().catch((error: unknown) =>
+            Logger.error("Capture failed", { error })
+          )
+        }
       />
       <Fab
         renderInPortal={false}
