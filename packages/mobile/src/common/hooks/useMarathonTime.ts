92139a985ff51b1a1b202151a999c0b8a60792a4
Joshua Tag Howard <tag@jthoward.dev>
2024-03-30 20:14:13 +0000

    Refactor useTabBarConfig and gql queries

--- packages/mobile/src/common/hooks/useMarathonTime.ts
+++ packages/mobile/src/common/hooks/useMarathonTime.ts
@@ -1,34 +1,20 @@
-import { SimpleConfigFragment } from "@common/fragments/Configuration";
-import { log, logError } from "@common/logging";
-import {
-  getFragmentData,
-  graphql,
-} from "@ukdanceblue/common/dist/graphql-client-public";
+import { Logger } from "@common/logger/Logger";
+import { dateTimeFromSomething } from "@ukdanceblue/common";
+import { graphql } from "@ukdanceblue/common/dist/graphql-client-public";
 import { DateTime } from "luxon";
 import { useEffect, useMemo } from "react";
 import { useQuery } from "urql";
 
 export interface MarathonTime {
-  startTime: DateTime,
-  endTime: DateTime,
+  startTime: DateTime;
+  endTime: DateTime;
 }
 
-const useMarathonStartQuery = graphql(/* GraphQL */ `
-  query useMarathonStart {
-    activeConfiguration(key: "MARATHON_START") {
-      data {
-        ...SimpleConfig
-      }
-    }
-  }
-`);
-
-const useMarathonEndQuery = graphql(/* GraphQL */ `
-  query useMarathonEnd {
-    activeConfiguration(key: "MARATHON_END") {
-      data {
-        ...SimpleConfig
-      }
+const marathonTimeQuery = graphql(/* GraphQL */ `
+  query MarathonTime {
+    nextMarathon {
+      startDate
+      endDate
     }
   }
 `);
@@ -37,68 +23,53 @@ export function useMarathonTime(): {
   timesLoading: boolean;
   marathonTime: MarathonTime;
 } {
-  const [{ data: startData, fetching: startFetch, error: startError }] = useQuery({
-    query: useMarathonStartQuery,
+  const [{ data, fetching, error }] = useQuery({
+    query: marathonTimeQuery,
   });
-
-  const startValue = getFragmentData(
-    SimpleConfigFragment,
-    startData?.activeConfiguration.data
-  );
-
-  const [{ data: endData, fetching: endFetch, error: endError }] = useQuery({
-    query: useMarathonEndQuery,
-  });
-
-  const endValue = getFragmentData(
-    SimpleConfigFragment,
-    endData?.activeConfiguration.data
-  );
 
   useEffect(() => {
-    if (startError) {
-      logError(startError);
-    }
-    if (endError) {
-      logError(endError);
+    if (error) {
+      Logger.error("Failed to fetch marathon start time", {
+        error,
+        source: "useMarathonTime",
+      });
     }
   });
 
-  const determineMarathonIntervals = useMemo(() => {
+  const marathonInterval = useMemo(() => {
     let startTime: DateTime = DateTime.fromMillis(0);
     let endTime: DateTime = DateTime.fromMillis(0);
 
     try {
-      if (startValue) {
-        const parsed = startValue.value;
-        startTime = DateTime.fromISO(parsed);
+      if (data?.nextMarathon) {
+        startTime = dateTimeFromSomething(data.nextMarathon.startDate);
         if (!startTime.isValid) {
-          log(`Unrecognized marathon start time: ${startTime.toString()}`, "warn");
+          Logger.warn(
+            `Unrecognized marathon start time: ${startTime.toString()}`,
+            {
+              source: "useMarathonTime",
+            }
+          );
         }
-      }
-
-      if (endValue) {
-        const parsed = endValue.value;
-        endTime = DateTime.fromISO(parsed);
+        endTime = dateTimeFromSomething(data.nextMarathon.endDate);
         if (!endTime.isValid) {
-          log(`Unrecognized marathon end time: ${endTime.toString()}`, "warn");
+          Logger.warn(`Unrecognized marathon end time: ${endTime.toString()}`, {
+            source: "useMarathonTime",
+          });
         }
       }
     } catch (error) {
-      if (error instanceof Error) {
-        logError(error);
-      } else {
-        log(String(error), "error");
-      }
+      Logger.error("Failed to determine marathon intervals", {
+        error,
+        source: "useMarathonTime",
+      });
     }
 
-    return({startTime, endTime} as MarathonTime);
-
-  }, [startValue, endValue]);
-
+    return { startTime, endTime } as MarathonTime;
+  }, [data?.nextMarathon]);
 
   return {
-    timesLoading: (startFetch || endFetch),
-    marathonTime: determineMarathonIntervals
+    timesLoading: fetching,
+    marathonTime: marathonInterval,
   };
 }
