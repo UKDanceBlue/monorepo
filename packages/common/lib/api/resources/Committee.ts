358376c8a9ceacd45dec0fc41487eb232a74280e
Joshua Tag Howard <tag@jthoward.dev>
2024-05-03 11:30:12 +0000

    chore: Update CommitteeNode to include identifier field

--- packages/common/lib/api/resources/Committee.ts
+++ packages/common/lib/api/resources/Committee.ts
@@ -1,5 +1,6 @@
 import { Field, ID, ObjectType } from "type-graphql";
 
+import { CommitteeIdentifier } from "../../authorization/structures.js";
 import { Node } from "../relay.js";
 
 import { TimestampedResource } from "./Resource.js";
@@ -8,6 +9,16 @@ import { TimestampedResource } from "./Resource.js";
 export class CommitteeNode extends TimestampedResource implements Node {
   @Field(() => ID)
   id!: string;
+
+  @Field(() => CommitteeIdentifier)
+  identifier!: CommitteeIdentifier;
+
+  static init(init: { id: string; identifier: CommitteeIdentifier }) {
+    return this.doInit({
+      id: init.id,
+      identifier: init.identifier,
+    });
+  }
 
   public getUniqueId(): string {
     return this.id;
