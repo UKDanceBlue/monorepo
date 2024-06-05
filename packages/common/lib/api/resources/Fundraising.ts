5a626301bc36a6b26a84b5cd005e35e8472aea02
Joshua Tag Howard <tag@jthoward.dev>
2024-06-05 21:24:05 +0000

    Fix fundraising node classes

--- packages/common/lib/api/resources/Fundraising.ts
+++ packages/common/lib/api/resources/Fundraising.ts
@@ -1,5 +1,7 @@
 import { DateTimeISOResolver } from "graphql-scalars";
 import type { DateTime } from "luxon";
+import { Maybe } from "true-myth";
+import { nothing, of } from "true-myth/maybe";
 import { Field, Float, ID, ObjectType } from "type-graphql";
 
 import { dateTimeFromSomething } from "../../utility/time/intervalTools.js";
@@ -13,14 +15,26 @@ import { TimestampedResource } from "./Resource.js";
 export class FundraisingEntryNode extends TimestampedResource implements Node {
   @Field(() => ID)
   id!: string;
-  @Field(() => String)
-  donatedByText!: string;
-  @Field(() => String)
-  donatedToText!: string;
+  @Field(() => String, { nullable: true, name: "donatedByText" })
+  private _donatedByText!: string | null;
+  get donatedByText(): Maybe<string> {
+    return of(this._donatedByText);
+  }
+  set donatedByText(value: Maybe<string>) {
+    this._donatedByText = value.unwrapOr(null);
+  }
+  @Field(() => String, { nullable: true, name: "donatedToText" })
+  private _donatedToText!: string | null;
+  get donatedToText(): Maybe<string> {
+    return of(this._donatedToText);
+  }
+  set donatedToText(value: Maybe<string>) {
+    this._donatedToText = value.unwrapOr(null);
+  }
   @Field(() => DateTimeISOResolver)
-  start!: Date;
-  get startDateTime(): DateTime {
-    return dateTimeFromSomething(this.start);
+  donatedOn!: Date;
+  get donatedOnDateTime(): DateTime {
+    return dateTimeFromSomething(this.donatedOn);
   }
   @Field(() => Float)
   amount!: number;
@@ -29,8 +43,35 @@ export class FundraisingEntryNode extends TimestampedResource implements Node {
     return this.id;
   }
 
-  public static init(init: Partial<FundraisingEntryNode>) {
-    return FundraisingEntryNode.doInit(init);
+  public static init(init: {
+    id: string;
+    donatedByText: Maybe<string> | string | null;
+    donatedToText: Maybe<string> | string | null;
+    donatedOn: Date;
+    amount: number;
+    createdAt: Date;
+    updatedAt: Date;
+  }) {
+    const node = new FundraisingEntryNode();
+    node.id = init.id;
+    node.donatedByText =
+      init.donatedByText == null
+        ? nothing()
+        : typeof init.donatedByText === "string"
+          ? of(init.donatedByText)
+          : init.donatedByText;
+    node.donatedToText =
+      init.donatedToText == null
+        ? nothing()
+        : typeof init.donatedToText === "string"
+          ? of(init.donatedToText)
+          : init.donatedToText;
+    node.donatedOn = init.donatedOn;
+    node.amount = init.amount;
+    node.createdAt = init.createdAt;
+    node.updatedAt = init.updatedAt;
+
+    return node;
   }
 }
 
@@ -56,7 +97,12 @@ export class FundraisingAssignmentNode
     return this.id;
   }
 
-  public static init(init: Partial<FundraisingAssignmentNode>) {
+  public static init(init: {
+    id: string;
+    amount: number;
+    createdAt: Date;
+    updatedAt: Date;
+  }) {
     return FundraisingAssignmentNode.doInit(init);
   }
 }
