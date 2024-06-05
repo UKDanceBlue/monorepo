f6b3fcced04feaba64f39862ca5b12a43fc6c400
Joshua Tag Howard <tag@jthoward.dev>
2024-06-05 20:55:19 +0000

    Add ControlError and related classes for error handling

---
+++ packages/server/src/lib/error/control.ts
@@ -0,0 +1,54 @@
+import type { AuthorizationRule } from "@ukdanceblue/common";
+import { prettyPrintAuthorizationRule } from "@ukdanceblue/common";
+
+import { ConcreteError } from "./error.js";
+
+/**
+ * These errors are caused when the server can do something, but doesn't want to. For example
+ * when a user tries to do something they aren't allowed to do, or when a notification is cancelled
+ * after it's already been sent.
+ *
+ * Exposed by default.
+ */
+export abstract class ControlError extends ConcreteError {
+  abstract get message(): string;
+  get detailedMessage(): string {
+    return this.message;
+  }
+  get expose() {
+    return true;
+  }
+  get stack(): string | undefined {
+    return undefined;
+  }
+}
+
+export class UnauthorizedError extends ControlError {
+  get message() {
+    return "Unauthorized";
+  }
+
+  constructor(protected readonly requiredAuthorization: AuthorizationRule[]) {
+    super();
+  }
+
+  get detailedMessage() {
+    return `Unauthorized: ${this.requiredAuthorization.map(prettyPrintAuthorizationRule).join(", ")}`;
+  }
+}
+
+export class UnauthenticatedError extends ControlError {
+  get message() {
+    return "Unauthenticated";
+  }
+}
+
+export class ActionDeniedError extends ControlError {
+  constructor(protected readonly action: string) {
+    super();
+  }
+
+  get message() {
+    return `Action denied: ${this.action}`;
+  }
+}
