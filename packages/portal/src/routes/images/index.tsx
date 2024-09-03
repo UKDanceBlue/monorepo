dc1cc468e4a413eac55a9c6a71d944fbd6360d7e
Joshua Tag Howard <tag@jthoward.dev>
2024-09-03 18:51:17 +0000

    Move images route

--- packages/portal/src/routes/images/index.tsx
+++ packages/portal/src/routes/images/index.tsx
@@ -1,5 +1,40 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { PlusOutlined } from "@ant-design/icons";
+import { CreateImagePopup } from "@elements/components/image/CreateImagePopup";
+import { ImagesTable } from "@elements/tables/ImagesTable";
+import { createFileRoute } from "@tanstack/react-router";
+import { Button, Flex, Typography } from "antd";
+import { useState } from "react";
 
-export const Route = createFileRoute('/images/')({
-  component: () => <div>Hello /images/!</div>
-})
+function ListImagesPage() {
+  const [createImageOpen, setCreateImageOpen] = useState(false);
+
+  return (
+    <>
+      <Flex justify="space-between" align="center">
+        <Typography.Title>Images</Typography.Title>
+        <Button
+          type="link"
+          icon={<PlusOutlined />}
+          onClick={() => setCreateImageOpen(true)}
+          size="large"
+        >
+          Add Image
+        </Button>
+      </Flex>
+      <CreateImagePopup
+        open={createImageOpen}
+        onClose={(createdImageUuid) => {
+          setCreateImageOpen(false);
+          if (createdImageUuid) {
+            // TODO: Navigate to the created image
+          }
+        }}
+      />
+      <ImagesTable />
+    </>
+  );
+}
+
+export const Route = createFileRoute("/images/")({
+  component: ListImagesPage,
+});
