d65adb4f63e5c6848fa7430d72c78fde56311f99
Joshua Tag Howard <tag@jthoward.dev>
2024-09-10 05:59:56 +0000

    EAS changes

--- packages/mobile/src/types/UnmaskFragment.ts
+++
@@ -1,6 +0,0 @@
-import type { DocumentTypeDecoration } from "@graphql-typed-document-node/core";
-
-export type UnmaskFragment<Unmasked> = Omit<
-  Unmasked extends DocumentTypeDecoration<infer TType, never> ? TType : never,
-  "__typename" | " $fragmentName"
->;
