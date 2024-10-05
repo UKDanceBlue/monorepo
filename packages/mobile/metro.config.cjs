550771dbf29f2ecbb12e85c0980224ff934c6af6
Joshua Tag Howard <tag@jthoward.dev>
2024-10-05 21:25:35 +0000

    Refactor metro.config.cjs to change how config is generated

--- packages/mobile/metro.config.cjs
+++ packages/mobile/metro.config.cjs
@@ -1,35 +1,43 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getSentryExpoConfig } = require("@sentry/react-native/metro");
+const { withSentryConfig } = require("@sentry/react-native/metro");
 const { getDefaultConfig } = require("expo/metro-config");
 
 // Find the project and workspace directories
 const projectRoot = __dirname;
 
+/** @type {import("metro-resolver").CustomResolver} */
+const resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === "type-graphql") {
+    return context.resolveRequest(context, "type-graphql/shim", platform);
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+const getTransformOptions = () =>
+  Promise.resolve({
+    transform: {
+      experimentalImportSupport: true,
+      inlineRequires: true,
+    },
+  });
+
 /** @return {import("expo/metro-config").MetroConfig} */
 const config = () => {
   /** @type {import("expo/metro-config").MetroConfig}*/
-  const config = getSentryExpoConfig(projectRoot, { getDefaultConfig });
+  const config = getDefaultConfig(projectRoot);
 
-  const oldResolver = config.resolver.resolveRequest;
-  /** @type {import("metro-resolver").CustomResolver} */
-  const resolveRequest = (context, moduleName, platform) => {
-    if (moduleName === "type-graphql") {
-      return context.resolveRequest(context, "type-graphql/shim", platform);
-    }
-    return oldResolver(context, moduleName, platform);
-  };
-  config.resolver.resolveRequest = resolveRequest;
-  config.transformer.getTransformOptions = () =>
-    Promise.resolve({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    });
-
-  config.resolver.unstable_enablePackageExports = true;
-
-  return config;
+  return withSentryConfig({
+    ...config,
+    resolver: {
+      ...config.resolver,
+      resolveRequest,
+      unstable_enablePackageExports: true,
+    },
+    transformer: {
+      ...config.transformer,
+      getTransformOptions,
+    },
+  });
 };
 
 module.exports = config();
