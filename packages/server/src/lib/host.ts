623e2c4b8ab4259c7c42076e11df0d2faffe241b
Joshua Tag Howard <tag@jthoward.dev>
2024-11-18 17:28:40 +0000

    Refactor Dockerfile and add zrok installation script

--- packages/server/src/lib/host.ts
+++ packages/server/src/lib/host.ts
@@ -1,7 +1,16 @@
+import { Container } from "@freshgum/typedi";
 import type { Request } from "express";
+
+import { applicationPortToken } from "./environmentTokens.js";
 
 export function getHostUrl(req: Request): URL {
   const { protocol, hostname } = req;
 
-  return new URL(`${protocol}://${hostname}`);
+  const applicationPort = Container.get(applicationPortToken);
+
+  return req.get("X-Forwarded-Proto") ||
+    (protocol === "https" && applicationPort === 443) ||
+    (protocol === "http" && applicationPort === 80)
+    ? new URL(`${protocol}://${hostname}`)
+    : new URL(`${protocol}://${hostname}:${applicationPort}`);
 }
