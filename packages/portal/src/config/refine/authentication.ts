daa54eccb1d3eed60800884423c580f7cf57ed91
Joshua Tag Howard <tag@jthoward.dev>
2024-12-02 18:04:17 +0000

    Refactor authentication flow to remove popup handling and use simple redirect

--- packages/portal/src/config/refine/authentication.ts
+++ packages/portal/src/config/refine/authentication.ts
@@ -9,33 +9,37 @@ import { API_BASE_URL, urqlClient } from "#config/api.ts";
 import { refreshLoginState } from "#hooks/useLoginState.ts";
 
 function openAuthPopup(path: "login" | "logout"): Promise<string> {
-  return new Promise((resolve, reject) => {
-    const popup = window.open(
-      `${API_BASE_URL}/api/auth/${path}?returning=cookie&redirectTo=${encodeURI(
-        new URL("#popup", window.location.origin).href
-      )}`,
-      undefined,
-      "popup"
-    );
-    if (!popup) {
-      reject(new Error("Failed to open popup"));
-      return;
-    }
+  return new Promise((resolve, _reject) => {
+    // const popup = window.open(
+    //   `${API_BASE_URL}/api/auth/${path}?returning=cookie&redirectTo=${encodeURI(
+    //     new URL("assets/popup.html", window.location.origin).href
+    //   )}`,
+    //   undefined,
+    //   "popup"
+    // );
+    // if (!popup) {
+    //   reject(new Error("Failed to open popup"));
+    //   return;
+    // }
 
-    addEventListener("message", (event) => {
-      console.log(event);
-      if (event.origin === window.location.origin) {
-        resolve(String(event.data));
-        popup.close();
-      }
-    });
-    popup.addEventListener("error", (event) => {
-      reject(new Error(event.message));
-      popup.close();
-    });
-    popup.addEventListener("beforeunload", () => {
-      reject(new Error("User closed the popup"));
-    });
+    // addEventListener("message", (event) => {
+    //   console.log(event);
+    //   if (event.origin === window.location.origin) {
+    //     resolve(String(event.data));
+    //     popup.close();
+    //   }
+    // });
+    // popup.addEventListener("error", (event) => {
+    //   reject(new Error(event.message));
+    //   popup.close();
+    // });
+    // popup.addEventListener("beforeunload", () => {
+    //   reject(new Error("User closed the popup"));
+    // });
+    window.location.href = `${API_BASE_URL}/api/auth/${path}?returning=cookie&redirectTo=${encodeURI(
+      window.location.href
+    )}`;
+    resolve("success");
   });
 }
 
