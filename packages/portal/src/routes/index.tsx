04a7bfeb5bd8d4111020754fe5476763a38e63fe
Joshua Tag Howard <tag@jthoward.dev>
2024-12-08 19:55:16 +0000

    Add "What's New?" section to HomePage with updates on UI changes and authorization system

--- packages/portal/src/routes/index.tsx
+++ packages/portal/src/routes/index.tsx
@@ -47,6 +47,29 @@ function HomePage() {
       <Typography.Title>
         Welcome to the DanceBlue online portal!
       </Typography.Title>
+      <Typography.Title level={2}>What's New?</Typography.Title>
+      <Typography.Paragraph>
+        <ul>
+          <li>This list!</li>
+          <li>
+            The portal has had few big changes to the user interface, namely the
+            menu moving to the left; but you will notice some changes to to
+            other pages as well. For now this mostly means the events pages, but
+            others will come soon.
+          </li>
+          <li>
+            The entire authorization system has been replaced, please contact
+            Tech Committee if you have any issues with "Access Denied" errors!
+            This change will make it a lot easier to fix these issues in the
+            future, but there may be some growing pains.
+          </li>
+          <li>
+            Any public events published to BBNvolved are now automatically
+            pulled into the app's database.
+          </li>
+          <li>More coming soon!</li>
+        </ul>
+      </Typography.Paragraph>
       {data?.me && (
         <>
           <Typography.Title level={2}>Your Information</Typography.Title>
