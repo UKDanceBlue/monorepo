ff2711db3035b0ecdb948da3d96193d6588b6ac4
Joshua Tag Howard <tag@jthoward.dev>
2025-01-29 19:26:44 +0000

    Enhance environment variable loading with error handling in EnvironmentService

--- packages/server/src/entry/environment/environment.ts
+++ packages/server/src/entry/environment/environment.ts
@@ -3,7 +3,11 @@
 import { Container } from "@freshgum/typedi";
 import { Expo } from "expo-server-sdk";
 
-import { type Environment, setEnvironment } from "#lib/typediTokens.js";
+import {
+  type Environment,
+  getEnvironment,
+  setEnvironment,
+} from "#lib/typediTokens.js";
 import { expoServiceToken } from "#notification/expoServiceToken.js";
 
 export abstract class EnvironmentService {
@@ -28,6 +32,11 @@ export abstract class EnvironmentService {
       new Expo({ accessToken: env.expoAccessToken })
     );
     setEnvironment(env);
+    try {
+      getEnvironment();
+    } catch (error) {
+      throw new Error("Error loading environment variables", { cause: error });
+    }
 
     // For third party libraries that rely on NODE_ENV
     process.env.NODE_ENV = env.NODE_ENV;
