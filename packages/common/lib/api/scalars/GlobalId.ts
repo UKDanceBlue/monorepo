335a8f55e066de2be8dd9b7162d8037c8e8c6a49
Joshua Tag Howard <tag@jthoward.dev>
2025-02-03 15:07:35 +0000

    Add UUID validation to GlobalIdScalar for improved parsing accuracy

--- packages/common/lib/api/scalars/GlobalId.ts
+++ packages/common/lib/api/scalars/GlobalId.ts
@@ -15,6 +15,8 @@ import {
   UTF8ArrToStr,
 } from "../../utility/primitive/base64.js";
 
+const uuidRegex = /^[\da-f]{8}(?:\b-[\da-f]{4}){3}\b-[\da-f]{12}$/i;
+
 export interface GlobalId {
   typename: string;
   id: string;
@@ -104,6 +106,9 @@ export const GlobalIdScalar = new GraphQLScalarType({
       const parsed = parseGlobalId(value);
       if (parsed.isErr()) {
         throw new TypeError(parsed.error.message);
+      }
+      if (!uuidRegex.test(parsed.value.id)) {
+        throw new TypeError("GlobalIdScalar can only parse valid UUIDs");
       }
       return parsed.value;
     } else {
