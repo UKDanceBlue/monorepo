99052f22b214365362427a691001d12dc3b6827a
Joshua Tag Howard <tag@jthoward.dev>
2025-02-26 15:24:57 +0000

    Refactor promotion workflows and scripts for improved clarity and efficiency

--- scripts/promote-eas-update.sh
+++ scripts/promote-eas-update.sh
@@ -37,24 +37,3 @@ else
 fi
 
 popd
-
-echo ⏬ Pulling images
-docker image pull ghcr.io/ukdanceblue/app-server:$FROM_BRANCH
-docker image pull ghcr.io/ukdanceblue/app-server:$TO_BRANCH
-
-# Check if the two images are the same already
-DIFFERENT=$(docker image inspect ghcr.io/ukdanceblue/app-server:$FROM_BRANCH --format='{{.Id}}' | grep $(docker image inspect ghcr.io/ukdanceblue/app-server:$TO_BRANCH --format='{{.Id}}') || true)
-
-if [ -z "$DIFFERENT" ]; then
-  echo ⏭️ Promoting image $FROM_BRANCH to $TO_BRANCH
-  docker image tag ghcr.io/ukdanceblue/app-server:$FROM_BRANCH ghcr.io/ukdanceblue/app-server:$TO_BRANCH
-  
-  docker image ls ghcr.io/ukdanceblue/app-server
-
-  echo ⏫ Pushing tagged image $TO_BRANCH
-  docker image push --all-tags ghcr.io/ukdanceblue/app-server
-
-  echo ✅ Promoted image $FROM_BRANCH to $TO_BRANCH
-else
-  echo ☑️ "No new image to promote"
-fi
