1505771c188579d482d8b899510dc6e965896a36
Joshua Tag Howard <tag@jthoward.dev>
2025-03-27 22:10:54 +0000

    Remove unused AsyncStorage calls from UrqlContext to streamline authentication handling

--- packages/mobile/src/context/urql.tsx
+++ packages/mobile/src/context/urql.tsx
@@ -73,11 +73,6 @@ export function UrqlContext({ children }: { children: ReactNode }) {
                     },
                   },
                 });
-                AsyncStorage.removeItem(DANCEBLUE_TOKEN_KEY).then(
-                  () => invalidate(),
-                  console.error
-                );
-                return true;
               } else if (
                 (response as { status?: unknown } | undefined)?.status ===
                   401 ||
@@ -95,7 +90,6 @@ export function UrqlContext({ children }: { children: ReactNode }) {
                     },
                   },
                 });
-                return true;
               }
 
               return false;
