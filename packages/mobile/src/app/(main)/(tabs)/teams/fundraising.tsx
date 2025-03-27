c82f628989233bd0397ca0efcaa3788da1f07127
Joshua Tag Howard <tag@jthoward.dev>
2025-03-27 21:01:26 +0000

    Add GraphQL query for fundraising data retrieval in Fundraising component

--- packages/mobile/src/app/(main)/(tabs)/teams/fundraising.tsx
+++ packages/mobile/src/app/(main)/(tabs)/teams/fundraising.tsx
@@ -1,3 +1,24 @@
+import { graphql } from "~/api";
+
+const FundraisingDocument = graphql(/* GraphQL */ `
+  query FundraisingQuery {
+    me {
+      fundraisingTotalAmount
+      fundraisingAssignments {
+        id
+        amount
+        entry {
+          donatedOn
+          amount
+          notes
+          donatedByText
+          donatedToText
+        }
+      }
+    }
+  }
+`);
+
 export default function Fundraising() {
   return null;
 }
