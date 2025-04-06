e3329be94b2adf6c59a4d924817dfe262bdb5299
Joshua Tag Howard <tag@jthoward.dev>
2025-04-06 18:00:37 +0000

    Create Marathon component; remove unused Hour component and layout files

---
+++ packages/mobile/src/app/(main)/(tabs)/marathon.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+import { Text } from "~/components/ui/text";
+
+export default function Marathon() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Marathon</Text>
+    </View>
+  );
+}
--- packages/mobile/src/app/(main)/(tabs)/marathon/[hour]/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Hour() {
-  return null;
-}
